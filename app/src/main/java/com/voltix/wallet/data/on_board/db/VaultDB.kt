5e7715146d042f53bc930288b745334135883089
aminsato <amin.saradar@yahoo.com>
2024-05-01 14:08:29 +0330

    Update VaultDB.kt

--- app/src/main/java/com/voltix/wallet/data/on_board/db/VaultDB.kt
+++ app/src/main/java/com/voltix/wallet/data/on_board/db/VaultDB.kt
@@ -1,27 +1,28 @@
 package com.voltix.wallet.data.on_board.db
+
 import com.voltix.wallet.models.Vault
 import android.content.Context
 import com.google.gson.Gson
-import java.util.concurrent.atomic.AtomicInteger
+import com.google.gson.JsonSyntaxException
 
 class VaultDB(private val context: Context) {
 
-    private val nextId = AtomicInteger(1)
     private val gson = Gson()
     private val vaultsFolder = context.filesDir.resolve("vaults")
+
     init {
         // Ensure the vaults folder exists
         vaultsFolder.mkdirs()
     }
 
     fun upsert(vault: Vault) {
-        val file = vaultsFolder.resolve("${vault.name}-vault.json")
+        val file = vaultsFolder.resolve("${vault.name}-vault.dat")
         file.writeText(gson.toJson(vault))
     }
 
     // Delete operation
     fun delete(vaultName: String) {
-        val file = vaultsFolder.resolve("${vaultName}-vault.json")
+        val file = vaultsFolder.resolve("${vaultName}-vault.dat")
         if (file.exists()) {
             file.delete()
         }
@@ -29,7 +30,7 @@ class VaultDB(private val context: Context) {
 
     // Select operation
     fun select(vaultName: String): Vault? {
-        val file = vaultsFolder.resolve("${vaultName}-vault.json")
+        val file = vaultsFolder.resolve("${vaultName}-vault.dat")
         return if (file.exists()) {
             gson.fromJson(file.readText(), Vault::class.java)
         } else {
@@ -41,9 +42,17 @@ class VaultDB(private val context: Context) {
         val allVaults = mutableListOf<Vault>()
         val vaultFiles = vaultsFolder.listFiles()
         vaultFiles?.forEach { file ->
-            val vaultJson = file.readText()
-            val vault = gson.fromJson(vaultJson, Vault::class.java)
-            allVaults.add(vault)
+            try {
+                val vaultJson = file.readText()
+                val vault = gson.fromJson(vaultJson, Vault::class.java)
+                allVaults.add(vault)
+            } catch (e: JsonSyntaxException) {
+                // Log the error or handle it as needed
+                println("Skipping file ${file.name}: not a valid Vault JSON")
+            } catch (e: Exception) {
+                // Handle other unexpected exceptions
+                println("Unexpected error while processing file ${file.name}: ${e.message}")
+            }
         }
         return allVaults
     }
