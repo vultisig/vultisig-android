5b875be31996e891c5e6c45b4009b779339cf7a7
Johnny Luo <johnnyluo1980@gmail.com>
2024-05-03 21:59:00 +1000

    Add a bunch implementation about for UTXO chain

--- app/src/main/java/com/voltix/wallet/app/VoltixApplication.kt
+++ app/src/main/java/com/voltix/wallet/app/VoltixApplication.kt
@@ -4,4 +4,9 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class VoltixApplication : Application()
+class VoltixApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        WalletCoreLoader
+    }
+}
