fa6ba03a2bd3fdfa1e7bedc865885344aa56ee32
Johnny Luo <johnnyluo1980@gmail.com>
2024-05-03 11:59:24 +1000

    downgrade minsdk to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.voltix.wallet"
-        minSdk = 34
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
