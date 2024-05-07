ae9dcb8901e0eef1f566700f8ad1764c1c5d5e58
aminsato <amin.saradar@yahoo.com>
2024-05-07 09:10:42 +0330

    remove redundant codes

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,4 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.daggerHiltAndroid) apply false
-
 }
