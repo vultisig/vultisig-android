d5cbc11329c0eb6961991448943205651b050760
aminsato <amin.saradar@yahoo.com>
2024-05-09 12:12:42 +0330

    Reapply "Resolved Conflicts"
    This reverts commit 5bc9cd70853af7d8462941bdb7ae6032b41bb893.

--- app/src/main/java/com/vultisig/wallet/ui/models/VaultDetailViewModel.kt
+++ app/src/main/java/com/vultisig/wallet/ui/models/VaultDetailViewModel.kt
@@ -1,12 +1,16 @@
 package com.vultisig.wallet.ui.models
 
 import androidx.annotation.DrawableRes
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.vultisig.wallet.data.on_board.db.VaultDB
 import com.vultisig.wallet.data.repositories.ChainAccountsRepository
 import com.vultisig.wallet.ui.models.mappers.ChainAccountToChainAccountUiModelMapper
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
@@ -32,12 +36,21 @@ internal class VaultDetailViewModel @Inject constructor(
     private val chainAccountsRepository: ChainAccountsRepository,
     private val chainAccountToUiModelMapper: ChainAccountToChainAccountUiModelMapper,
 ) : ViewModel() {
+    var isRefreshing: Boolean by mutableStateOf(false)
 
     val uiState = MutableStateFlow(VaultDetailUiModel())
 
     fun loadData(vaultId: String) {
         loadVaultName(vaultId)
         loadAccounts(vaultId)
+    }
+    fun refreshData() {
+        viewModelScope.launch {
+            isRefreshing = true
+            // TODO: add refresh logic here
+            delay(3000)
+            isRefreshing = false
+        }
     }
 
     private fun loadVaultName(vaultId: String) {
