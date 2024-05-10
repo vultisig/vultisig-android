70b3930def9f1ef63fe72cf7b2c1aa8394a5ca34
Yuri Golubev <14962060+YGolybev@users.noreply.github.com>
2024-05-10 20:38:53 +0400

    Fix provideAppDataStore

--- app/src/main/java/com/vultisig/wallet/data/DataModule.kt
+++ app/src/main/java/com/vultisig/wallet/data/DataModule.kt
@@ -1,5 +1,6 @@
 package com.vultisig.wallet.data
 
+import android.content.Context
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.vultisig.wallet.data.sources.AppDataStore
@@ -9,10 +10,10 @@ import com.vultisig.wallet.presenter.keysign.BlockChainSpecificSerializer
 import com.vultisig.wallet.presenter.keysign.KeysignPayload
 import com.vultisig.wallet.presenter.keysign.KeysignPayloadDeserializer
 import com.vultisig.wallet.presenter.keysign.KeysignPayloadSerializer
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
@@ -21,12 +22,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 internal interface DataModule {
-
-    @Binds
-    @Singleton
-    fun provideAppDataStore(
-        impl: AppDataStore
-    ): AppDataStore
 
     companion object {
 
@@ -72,6 +67,12 @@ internal interface DataModule {
         @Provides
         @Singleton
         fun provideHttpClient(): HttpClient = HttpClient(OkHttp)
+
+        @Provides
+        @Singleton
+        fun provideAppDataStore(
+            @ApplicationContext context: Context
+        ): AppDataStore = AppDataStore(context)
 
     }
 
