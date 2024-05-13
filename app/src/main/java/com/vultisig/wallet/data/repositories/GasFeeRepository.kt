fbc09d3f9aa1a7990268a5345549e0b8b6a3c534
Yuri Golubev <14962060+YGolybev@users.noreply.github.com>
2024-05-13 15:58:48 +0400

    Add gas fee calculation on a Send screen for THORChain, Maya, Gaia and Kujira

--- app/src/main/java/com/vultisig/wallet/data/repositories/GasFeeRepository.kt
+++ app/src/main/java/com/vultisig/wallet/data/repositories/GasFeeRepository.kt
@@ -1,5 +1,6 @@
 package com.vultisig.wallet.data.repositories
 
+import com.vultisig.wallet.chains.THORCHainHelper
 import com.vultisig.wallet.data.api.BlockChairApi
 import com.vultisig.wallet.data.api.EvmApiFactory
 import com.vultisig.wallet.data.models.GasFee
@@ -42,7 +43,31 @@ internal class GasFeeRepositoryImpl @Inject constructor(
             )
         }
 
-        else -> throw IllegalArgumentException("Can't estimate gas fee. Chain $chain is unsupported")
+        else -> when (chain) {
+            Chain.thorChain, Chain.mayaChain -> {
+                val nativeToken = tokenRepository.getNativeToken(chain.id).first()
+                GasFee(
+                    unit = chain.feeUnit,
+                    value = TokenValue(
+                        value = THORCHainHelper.THORChainGasUnit.toBigInteger(),
+                        decimals = nativeToken.decimal,
+                    )
+                )
+            }
+
+            Chain.gaiaChain, Chain.kujira -> {
+                val nativeToken = tokenRepository.getNativeToken(chain.id).first()
+                GasFee(
+                    unit = chain.feeUnit,
+                    value = TokenValue(
+                        value = 7500.toBigInteger(),
+                        decimals = nativeToken.decimal,
+                    )
+                )
+            }
+
+            else -> throw IllegalArgumentException("Can't estimate gas fee. Chain $chain is unsupported")
+        }
     }
 
 }
