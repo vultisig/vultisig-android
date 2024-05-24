58abfa4747fd00f33128e32a5b993dfff86a42cb
aminsato <amin.saradar@yahoo.com>
2024-05-24 11:58:40 +0330

    Rsolove requested changes

--- app/src/main/java/com/vultisig/wallet/ui/models/NamingVaultViewModel.kt
+++ app/src/main/java/com/vultisig/wallet/ui/models/NamingVaultViewModel.kt
@@ -3,19 +3,17 @@ package com.vultisig.wallet.ui.models
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.vultisig.wallet.ui.navigation.Destination
+import com.vultisig.wallet.ui.navigation.Destination.KeygenFlow.Companion.DEFAULT_NEW_VAULT
 import com.vultisig.wallet.ui.navigation.Navigator
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 internal data class NamingVaultUiModel(
-    val name: String = "",
-    val isContinueDisabled: Boolean = true
+    val name: String = ""
 )
 
 @HiltViewModel
@@ -25,17 +23,14 @@ internal class NamingVaultViewModel @Inject constructor(
     private val _uiModel = MutableStateFlow(NamingVaultUiModel())
     val uiModel = _uiModel.asStateFlow()
 
-    private val _snackBarChannel = Channel<String?>()
-    val snackBarChannel = _snackBarChannel.receiveAsFlow()
-
-
     fun onNameChanged(newName: String) {
-        _uiModel.update { it.copy(name = newName, isContinueDisabled = false) }
+        _uiModel.update { it.copy(name = newName) }
     }
 
     fun onContinueClick() {
         viewModelScope.launch {
-            navigator.navigate(Destination.KeygenFlow(uiModel.value.name))
+            navigator.navigate(Destination.KeygenFlow(
+                uiModel.value.name.takeIf { it.isNotEmpty() } ?: DEFAULT_NEW_VAULT))
         }
     }
 }
