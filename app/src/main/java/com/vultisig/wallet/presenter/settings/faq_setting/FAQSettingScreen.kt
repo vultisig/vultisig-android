0f0efffc28a9b9e487dc12ebe2c9c023d7403f68
Amin <32006742+aminsato@users.noreply.github.com>
2024-05-28 19:26:53 +0330

    Add FAQ answers (#251)
    * Add faq answers
    
    * Update FAQSettingScreen.kt

--- app/src/main/java/com/vultisig/wallet/presenter/settings/faq_setting/FAQSettingScreen.kt
+++ app/src/main/java/com/vultisig/wallet/presenter/settings/faq_setting/FAQSettingScreen.kt
@@ -1,25 +1,28 @@
 package com.vultisig.wallet.presenter.settings.faq_setting
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -27,6 +30,7 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.vultisig.wallet.R
+import com.vultisig.wallet.ui.components.FormCard
 import com.vultisig.wallet.ui.components.TopBar
 import com.vultisig.wallet.ui.theme.Theme
 
@@ -52,17 +56,11 @@ fun FAQSettingScreen(navController: NavHostController) {
             modifier = Modifier
                 .padding(it)
                 .padding(horizontal = 12.dp),
+            contentPadding = PaddingValues(bottom = 12.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(12.dp),
         ) {
-
-            items(state.questions) { q ->
-                FAQSettingItem(
-                    question = q.text
-                ) {
-
-                }
-            }
+            items(state.questions) { faq -> FAQSettingItem(faq = faq) }
         }
 
     }
@@ -70,26 +68,27 @@ fun FAQSettingScreen(navController: NavHostController) {
 
 
 @Composable
-private fun FAQSettingItem(question: String, onClick: () -> Unit = {}) {
+private fun FAQSettingItem(faq: Faq) {
     val colors = Theme.colors
-    Card(
-        modifier = Modifier
-            .clickable(onClick = onClick)
-            .padding(horizontal = 12.dp, vertical = 8.dp)
-            .fillMaxWidth(),
-        shape = RoundedCornerShape(10.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = colors.oxfordBlue600Main
-        )
-    ) {
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
+    val rotation = animateFloatAsState(
+        targetValue = if (isExpanded) 90f else 0f, label = "arrow angle"
+    )
+    FormCard(modifier = Modifier.animateContentSize()) {
         Row(
-            modifier = Modifier.padding(all = 12.dp),
+            modifier = Modifier
+                .padding(all = 12.dp)
+                .clickable {
+                    isExpanded = !isExpanded
+                },
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
 
             Text(
-                text = question,
+                text = faq.question,
                 color = colors.neutral0,
                 style = Theme.montserrat.body2,
                 lineHeight = 25.sp,
@@ -97,10 +96,26 @@ private fun FAQSettingItem(question: String, onClick: () -> Unit = {}) {
             )
 
             Icon(
+                modifier = Modifier.rotate(rotation.value),
                 painter = painterResource(id = R.drawable.caret_right),
                 contentDescription = null,
                 tint = colors.neutral0,
             )
         }
+        if (isExpanded)
+            Row(
+                modifier = Modifier
+                    .padding(all = 12.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = faq.answer,
+                    color = colors.neutral0,
+                    style = Theme.montserrat.body2,
+                    lineHeight = 25.sp,
+                    modifier = Modifier.weight(0.8f)
+                )
+            }
     }
 }
