00a270e4f284587d893431b9563195b9c50bcf39
aminsato <amin.saradar@yahoo.com>
2024-05-29 11:59:49 +0330

    Update devices images

--- app/src/main/java/com/vultisig/wallet/ui/models/keygen/KeygenSetupViewModel.kt
+++ app/src/main/java/com/vultisig/wallet/ui/models/keygen/KeygenSetupViewModel.kt
@@ -16,17 +16,17 @@ internal data class KeygenSetupUiModel(
         KeygenSetupTabUiModel(
             title = R.string.s_of_s_vault.asUiText("2", "2"),
             content = R.string.setup_device_of_vault.asUiText("2"),
-            drawableResId = R.drawable.devices,
+            drawableResId = R.drawable.devices_two_two,
         ),
         KeygenSetupTabUiModel(
             title = R.string.s_of_s_vault.asUiText("2", "3"),
             content = R.string.setup_device_of_vault.asUiText("3"),
-            drawableResId = R.drawable.devices,
+            drawableResId = R.drawable.devices_two_three,
         ),
         KeygenSetupTabUiModel(
             title = R.string.s_of_s_vault.asUiText("M", "N"),
             content = R.string.setup_device_of_vault.asUiText("N"),
-            drawableResId = R.drawable.devices,
+            drawableResId = R.drawable.devices_m_n,
         ),
     )
 )
