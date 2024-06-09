1caaef67f00d30cdce219c917d4999ddffa237ec
aminsato <amin.saradar@yahoo.com>
2024-06-09 09:55:12 +0330

    Make localizable string resourse

--- app/src/main/java/com/vultisig/wallet/ui/components/DigitString.kt
+++ app/src/main/java/com/vultisig/wallet/ui/components/DigitString.kt
@@ -3,23 +3,17 @@ package com.vultisig.wallet.ui.components
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.vultisig.wallet.R
 
 @Composable
 internal fun digitStringToWords(@StringRes id: Int, vararg formatArgs: Any): String {
     val resources = LocalContext.current.resources;
     val formattedString = resources.getString(id, *formatArgs)
+    val digitWords = resources.getStringArray(R.array.digit_words)
+
     return formattedString.map {
         when (it) {
-            '0' -> "zero"
-            '1' -> "one"
-            '2' -> "two"
-            '3' -> "three"
-            '4' -> "four"
-            '5' -> "five"
-            '6' -> "six"
-            '7' -> "seven"
-            '8' -> "eight"
-            '9' -> "nine"
+            in '0'..'9' -> digitWords[it.toString().toInt()]
             else -> it.toString()
         }
     }.joinToString("")
