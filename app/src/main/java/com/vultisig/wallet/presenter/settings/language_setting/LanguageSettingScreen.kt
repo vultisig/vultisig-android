f42fcc317ceade4b83ea9568953a1a313697ae9e
aminsato <amin.saradar@yahoo.com>
2024-06-15 12:36:36 +0330

    update delete vault screen

--- app/src/main/java/com/vultisig/wallet/presenter/settings/language_setting/LanguageSettingScreen.kt
+++ app/src/main/java/com/vultisig/wallet/presenter/settings/language_setting/LanguageSettingScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
