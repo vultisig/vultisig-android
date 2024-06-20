c6f208c627da797c9e7faf84930a9a83a33b8dee
aminsato <amin.saradar@yahoo.com>
2024-06-20 11:07:29 +0330

    Add last key gen page

--- app/src/main/java/com/vultisig/wallet/ui/components/library/UiCircularProgressIndicator.kt
+++ app/src/main/java/com/vultisig/wallet/ui/components/library/UiCircularProgressIndicator.kt
@@ -8,16 +8,30 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.semantics.ProgressBarRangeInfo
 import androidx.compose.ui.semantics.progressBarRangeInfo
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
+import com.vultisig.wallet.R
 import com.vultisig.wallet.ui.theme.Theme
+import kotlin.math.PI
+import kotlin.math.sin
 
 @Composable
 internal fun UiCircularProgressIndicator(
@@ -42,6 +56,42 @@ internal fun UiCircularProgressIndicator(
         )
     }
 
+    val targetScale = 0.30f
+    val progressToStartScaling = 0.75f
+
+    val doneOpacity = coercedProgress().takeIf { it >= progressToStartScaling } ?: 0f
+
+    val circlesScale = if (coercedProgress() > progressToStartScaling) lerp(
+        1.0f,
+        stop = targetScale,
+        (1.0f - targetScale).div(1.0f - progressToStartScaling)
+            .times(coercedProgress() - progressToStartScaling)
+    ) else 1.0f
+
+
+    val doneCheckBounceAngel = if ((coercedProgress() >= progressToStartScaling))
+        (140 / (1 - progressToStartScaling)).times(coercedProgress() - progressToStartScaling)
+    else 0.0f
+
+    val tickScale = if (coercedProgress() > progressToStartScaling)
+        (coercedProgress() - progressToStartScaling).div(1 - progressToStartScaling).times(
+            //simulate bounce effect
+            sin(doneCheckBounceAngel.times(PI).div(180))
+        ).toFloat()
+    else 0f
+
+    val tickCircleColor = Theme.colors.turquoise600Main.copy(
+        alpha = if (coercedProgress() > progressToStartScaling)
+            (coercedProgress() - progressToStartScaling).div(1.0f - progressToStartScaling)
+        else 0.5f
+    )
+
+    val tickVector = ImageVector.vectorResource(id = R.drawable.done_check)
+    val tickPainter = rememberVectorPainter(image = tickVector)
+    val doneTextMeasurer = rememberTextMeasurer()
+    val doneText = stringResource(R.string.circular_progress_indicator_done)
+    val doneTextStyle =
+        Theme.menlo.heading5.copy(color = Theme.colors.neutral0.copy(alpha = doneOpacity))
     val fullCircle = 360f
 
     Canvas(
@@ -56,24 +106,75 @@ internal fun UiCircularProgressIndicator(
         val diameterOffset = stroke.width / 2
         val arcDimen = size.width - 2 * diameterOffset
 
-        drawArc(
-            color = trackColor,
-            startAngle = startAngle,
-            sweepAngle = fullCircle,
-            useCenter = false,
-            topLeft = Offset(diameterOffset, diameterOffset),
-            size = Size(arcDimen, arcDimen),
-            style = stroke,
-        )
+        val doneCheckCircleScale =
+            1 - circlesScale + if (coercedProgress() > progressToStartScaling)
+                diameterOffset.times(2).div(arcDimen) else 0f
+        val spaceBetweenDoneCircleAndText = 50
 
-        drawArc(
-            startAngle = startAngle,
-            sweepAngle = sweep,
-            useCenter = false,
-            topLeft = Offset(diameterOffset, diameterOffset),
-            size = Size(arcDimen, arcDimen),
-            style = stroke,
-            brush = indicatorBrush,
+
+        scale(circlesScale) {
+            drawArc(
+                color = trackColor,
+                startAngle = startAngle,
+                sweepAngle = fullCircle,
+                useCenter = false,
+                topLeft = Offset(diameterOffset, diameterOffset),
+                size = Size(arcDimen, arcDimen),
+                style = stroke,
+            )
+        }
+
+
+        scale(circlesScale) {
+            drawArc(
+                startAngle = startAngle,
+                sweepAngle = sweep,
+                useCenter = false,
+                topLeft = Offset(diameterOffset, diameterOffset),
+                size = Size(arcDimen, arcDimen),
+                style = stroke,
+                brush = indicatorBrush,
+            )
+        }
+
+
+        scale(doneCheckCircleScale) {
+            drawArc(
+                startAngle = startAngle,
+                sweepAngle = fullCircle,
+                useCenter = false,
+                topLeft = Offset(diameterOffset, diameterOffset),
+                size = Size(arcDimen, arcDimen),
+                style = Fill,
+                brush = SolidColor(tickCircleColor),
+            )
+        }
+
+
+        scale(tickScale) {
+            val tickSize = 250f
+            translate(
+                left = arcDimen.div(2) - tickSize.div(2) + diameterOffset,
+                top = arcDimen.div(2) - tickSize.div(2) + diameterOffset
+            ) {
+                with(tickPainter) {
+                    draw(Size(tickSize, tickSize), alpha = coercedProgress())
+                }
+            }
+        }
+
+        drawText(
+            textMeasurer = doneTextMeasurer,
+            text = doneText,
+            style = doneTextStyle,
+            topLeft = Offset(
+                x = center.x - doneTextMeasurer.measure(doneText, doneTextStyle).size.width.div(2),
+                y = center.y - doneTextMeasurer.measure(
+                    doneText,
+                    doneTextStyle
+                ).size.height.div(2) + arcDimen.times(targetScale).times(coercedProgress())
+                    .plus(spaceBetweenDoneCircleAndText),
+            )
         )
     }
 }
