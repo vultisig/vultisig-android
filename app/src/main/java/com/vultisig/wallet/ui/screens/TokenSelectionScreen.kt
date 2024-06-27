377ec40ab37b1f2e31ddf5d85a7d04324f09cf67
ybeve <14962060+yvebe@users.noreply.github.com>
2024-06-27 17:17:48 +0400

    Add separation between selected & other tokens in TokenSelectionScreen

--- app/src/main/java/com/vultisig/wallet/ui/screens/TokenSelectionScreen.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/TokenSelectionScreen.kt
@@ -1,12 +1,15 @@
 package com.vultisig.wallet.ui.screens
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -17,10 +20,12 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.vultisig.wallet.R
+import com.vultisig.wallet.models.Coin
 import com.vultisig.wallet.models.Coins
 import com.vultisig.wallet.ui.components.TokenSelectionItem
 import com.vultisig.wallet.ui.components.TopBar
 import com.vultisig.wallet.ui.models.TokenSelectionViewModel
+import com.vultisig.wallet.ui.models.TokenUiModel
 import com.vultisig.wallet.ui.theme.Theme
 
 @Composable
@@ -45,24 +50,58 @@ internal fun TokenSelectionScreen(
             contentPadding = PaddingValues(all = 16.dp),
             verticalArrangement = Arrangement.spacedBy(12.dp),
         ) {
-            items(state.tokens) { token ->
-                val coin = token.coin
-                TokenSelectionItem(
-                    title = coin.ticker,
-                    subtitle = coin.chain.raw,
-                    logo = Coins.getCoinLogo(logoName = coin.logo),
-                    isChecked = token.isEnabled,
-                    onCheckedChange = { checked ->
-                        if (checked) {
-                            viewModel.enableToken(coin)
-                        } else {
-                            viewModel.disableToken(coin)
-                        }
-                    }
-                )
-            }
+            TokensSection(
+                title = R.string.token_selection_selected_tokens_title,
+                tokens = state.selectedTokens,
+                onEnableToken = viewModel::enableToken,
+                onDisableToken = viewModel::disableToken
+            )
+            TokensSection(
+                title = R.string.token_selection_tokens_title,
+                tokens = state.otherTokens,
+                onEnableToken = viewModel::enableToken,
+                onDisableToken = viewModel::disableToken
+            )
         }
     }
+}
+
+private fun LazyListScope.TokensSection(
+    @StringRes title: Int,
+    tokens: List<TokenUiModel>,
+    onEnableToken: (Coin) -> Unit,
+    onDisableToken: (Coin) -> Unit,
+) {
+    item {
+        TokensTitle(text = stringResource(id = title))
+    }
+    items(tokens) { token ->
+        val coin = token.coin
+        TokenSelectionItem(
+            title = coin.ticker,
+            subtitle = coin.chain.raw,
+            logo = Coins.getCoinLogo(logoName = coin.logo),
+            isChecked = token.isEnabled,
+            onCheckedChange = { checked ->
+                if (checked) {
+                    onEnableToken(coin)
+                } else {
+                    onDisableToken(coin)
+                }
+            }
+        )
+    }
+}
+
+@Composable
+private fun TokensTitle(
+    text: String,
+) {
+    Text(
+        text = text.uppercase(),
+        color = Theme.colors.neutral300,
+        style = Theme.montserrat.body2,
+    )
 }
 
 @Preview
