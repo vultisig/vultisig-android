1e3470173c5abf8f5e36f3e3741fc49b47c80328
aminsato <amin.saradar@yahoo.com>
2024-07-03 10:13:15 +0330

    Update CurrencyUnitSettingScreen.kt

--- app/src/main/java/com/vultisig/wallet/presenter/settings/currency_unit_setting/CurrencyUnitSettingScreen.kt
+++ app/src/main/java/com/vultisig/wallet/presenter/settings/currency_unit_setting/CurrencyUnitSettingScreen.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -56,7 +58,9 @@ fun CurrencyUnitSettingScreen(navController: NavHostController) {
         }
     ) {
         Column(
-            modifier = Modifier.padding(it),
+            modifier = Modifier
+                .padding(it)
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             state.currencyUnits.forEach { currencyUnit ->
