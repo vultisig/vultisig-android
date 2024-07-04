5ac5c2a89e01c9d55a88db244a2572d5d3a55f24
aminsato <amin.saradar@yahoo.com>
2024-07-04 10:30:32 +0330

    Update LanguageSettingScreen.kt

--- app/src/main/java/com/vultisig/wallet/presenter/settings/language_setting/LanguageSettingScreen.kt
+++ app/src/main/java/com/vultisig/wallet/presenter/settings/language_setting/LanguageSettingScreen.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -57,7 +59,9 @@ fun LanguageSettingScreen(navController: NavHostController) {
         }
     ) {
         Column(
-            modifier = Modifier.padding(it),
+            modifier = Modifier
+                .padding(it)
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             state.languages.forEach { language ->
