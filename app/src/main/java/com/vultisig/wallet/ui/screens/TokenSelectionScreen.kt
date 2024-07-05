fe73d7e31bbe3d95708c77d7009b25c6ebad12c5
yvebe <14962060+yvebe@users.noreply.github.com>
2024-07-05 13:06:08 +0300

    Add search to chain selection screen (#534)

--- app/src/main/java/com/vultisig/wallet/ui/screens/TokenSelectionScreen.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/TokenSelectionScreen.kt
@@ -8,9 +8,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
@@ -20,10 +18,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -32,12 +28,10 @@ import androidx.navigation.compose.rememberNavController
 import com.vultisig.wallet.R
 import com.vultisig.wallet.models.Coin
 import com.vultisig.wallet.models.Coins
+import com.vultisig.wallet.ui.components.FormSearchBar
 import com.vultisig.wallet.ui.components.TokenSelectionItem
 import com.vultisig.wallet.ui.components.TopBar
-import com.vultisig.wallet.ui.components.UiIcon
 import com.vultisig.wallet.ui.components.UiSpacer
-import com.vultisig.wallet.ui.components.library.form.BasicFormTextField
-import com.vultisig.wallet.ui.components.library.form.FormCard
 import com.vultisig.wallet.ui.models.TokenSelectionUiModel
 import com.vultisig.wallet.ui.models.TokenSelectionViewModel
 import com.vultisig.wallet.ui.models.TokenUiModel
@@ -84,38 +78,13 @@ internal fun TokenSelectionScreen(
 
         UiSpacer(size = 8.dp)
 
-        FormCard(
+        FormSearchBar(
+            textFieldState = searchTextFieldState,
             modifier = Modifier
                 .padding(
                     horizontal = 16.dp,
-                ),
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .padding(
-                        horizontal = 12.dp,
-                    )
-            ) {
-                UiIcon(
-                    drawableResId = R.drawable.ic_search,
-                    size = 24.dp,
-                    tint = Theme.colors.neutral500
                 )
-
-                BasicFormTextField(
-                    textFieldState = searchTextFieldState,
-                    hint = stringResource(R.string.token_selection_search_hint),
-                    keyboardType = KeyboardType.Text,
-                    onLostFocus = {
-                        // todo no validation neeeded
-                    },
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .fillMaxWidth(),
-                )
-            }
-        }
+        )
 
         UiSpacer(size = 8.dp)
 
