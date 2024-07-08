3ad9d424f1f5a49f31f97fea9ed29cb98037fc54
aminsato <amin.saradar@yahoo.com>
2024-07-08 12:48:08 +0330

    fix-Import-Encrypted-Backup-IOS

--- app/src/main/java/com/vultisig/wallet/common/CryptoManager.kt
+++ app/src/main/java/com/vultisig/wallet/common/CryptoManager.kt
@@ -1,38 +1,74 @@
 package com.vultisig.wallet.common
 
-import android.util.Base64
 import timber.log.Timber
 import java.security.MessageDigest
+import java.security.SecureRandom
+import java.util.Base64
 import javax.crypto.BadPaddingException
 import javax.crypto.Cipher
+import javax.crypto.spec.GCMParameterSpec
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 import javax.inject.Inject
+import kotlin.text.Charsets.UTF_8
 
 
 internal interface CryptoManager {
-    fun encrypt(strToEncrypt: String, key: String): String
-    fun decrypt(key: String, dataToDecrypt: ByteArray): String?
+    fun encrypt(plainText: String, password: String): String?
+    fun decrypt(data: String, password: String): String?
 }
 
 
 internal class AESCryptoManager @Inject constructor() : CryptoManager {
 
-    private val cipher = Cipher.getInstance("AES/CBC/PKCS7PADDING")
-    private val ivParameterSpec = IvParameterSpec(ByteArray(16))
+    private val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+    private val messageDigest = MessageDigest.getInstance("SHA-256")
+    private val algorithm = "AES"
 
-    override fun encrypt(strToEncrypt: String, key: String): String {
-        val plainText = strToEncrypt.toByteArray(Charsets.UTF_8)
-        cipher.init(Cipher.ENCRYPT_MODE, generateKey(key), ivParameterSpec)
-        val cipherText = cipher.doFinal(plainText)
-        return Base64.encodeToString(cipherText, Base64.DEFAULT)
+
+    override fun encrypt(plainText: String, password: String): String? {
+        try {
+            val data = plainText.toByteArray(UTF_8)
+            val keyBytes = messageDigest.digest(password.toByteArray(UTF_8))
+            val keySpec = SecretKeySpec(keyBytes, algorithm)
+            val iv = ByteArray(12)
+            SecureRandom().nextBytes(iv)
+            val parameterSpec = GCMParameterSpec(128, iv)
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec, parameterSpec)
+            val encryptedData = cipher.doFinal(data)
+            val combined = iv + encryptedData
+            return Base64.getEncoder().encodeToString(combined)
+        } catch (e: Exception) {
+            Timber.e(e, "Error encrypting data: ${e.localizedMessage}")
+            return null
+        }
     }
 
-    override fun decrypt(key: String, dataToDecrypt: ByteArray): String? {
+
+    override fun decrypt(data: String, password: String): String? {
         try {
-            val textToDecrypt = Base64.decode(dataToDecrypt, Base64.DEFAULT)
-            cipher.init(Cipher.DECRYPT_MODE, generateKey(key), ivParameterSpec)
-            val cipherText = cipher.doFinal(textToDecrypt)
+            val keyBytes = messageDigest.digest(password.toByteArray(UTF_8))
+            val keySpec = SecretKeySpec(keyBytes, algorithm)
+            val cipherData = Base64.getDecoder().decode(data)
+            val iv = cipherData.copyOfRange(0, 12)
+            val encryptedData = cipherData.copyOfRange(12, cipherData.size)
+            cipher.init(Cipher.DECRYPT_MODE, keySpec, GCMParameterSpec(128, iv))
+            return cipher.doFinal(encryptedData).buildString()
+        } catch (e: Exception) {
+            Timber.e(e, "switch to old version decryption")
+            return decryptOldVersion(data = data.toByteArray(), password = password)
+        }
+    }
+
+
+    private fun decryptOldVersion(data: ByteArray, password: String): String? {
+        try {
+            val textToDecrypt = android.util.Base64.decode(data, android.util.Base64.DEFAULT)
+            val oldCipher = Cipher.getInstance("AES/CBC/PKCS7PADDING")
+            oldCipher.init(
+                Cipher.DECRYPT_MODE, generateKey(password), IvParameterSpec(ByteArray(16))
+            )
+            val cipherText = oldCipher.doFinal(textToDecrypt)
             return cipherText.buildString()
         } catch (e: BadPaddingException) {
             Timber.e(e, "Failed to decrypt data")
