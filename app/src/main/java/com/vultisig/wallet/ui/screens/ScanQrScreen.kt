f94bd51593b3560e184f0493422fb243ad055272
yevhen1sec <second.chance02.30@gmail.com>
2024-07-17 12:57:07 +0200

    code clean

--- app/src/main/java/com/vultisig/wallet/ui/screens/ScanQrScreen.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/ScanQrScreen.kt
@@ -2,12 +2,7 @@
 
 package com.vultisig.wallet.ui.screens
 
-import android.content.ContentResolver
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Color
 import android.net.Uri
 import android.util.Size
 import androidx.activity.compose.rememberLauncherForActivityResult
