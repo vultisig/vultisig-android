08e1c924cde89676de99489a4a8c6f91aa09836c
Amin <32006742+aminsato@users.noreply.github.com>
2024-07-22 15:02:50 +0330

    Resolved the issue with scanning iOS QR code content for BCH transactions. (#596)
    * Update Chain.kt
    
    * Add migration for chain refactor

--- app/src/main/java/com/vultisig/wallet/data/db/AppDatabase.kt
+++ app/src/main/java/com/vultisig/wallet/data/db/AppDatabase.kt
@@ -25,7 +25,7 @@ import com.vultisig.wallet.data.db.models.VaultOrderEntity
         TokenPriceEntity::class,
         AddressBookEntryEntity::class,
     ],
-    version = 7,
+    version = 8,
     exportSchema = false,
 )
 internal abstract class AppDatabase : RoomDatabase() {
