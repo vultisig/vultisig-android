c395ce6c396ecbf593b7fefa04981e5bd4569551
aminsato <amin.saradar@yahoo.com>
2024-07-24 15:37:06 +0330

    Revert change in DatabaseModule

--- app/src/main/java/com/vultisig/wallet/data/db/DatabaseModule.kt
+++ app/src/main/java/com/vultisig/wallet/data/db/DatabaseModule.kt
@@ -37,13 +37,8 @@ internal interface DatabaseModule {
                 name = DB_NAME,
             )
                 .addMigrations(
-                    MIGRATION_1_2,
-                    MIGRATION_2_3,
-                    MIGRATION_3_4,
-                    MIGRATION_4_5,
-                    MIGRATION_5_6,
-                    MIGRATION_6_7,
-                    MIGRATION_7_8,
+                    MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4, MIGRATION_4_5,
+                    MIGRATION_5_6, MIGRATION_6_7, MIGRATION_7_8,
                 )
                 .build()
 
