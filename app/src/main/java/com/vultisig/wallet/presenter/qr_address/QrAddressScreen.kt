45732530f2abf834208d9529c466777063b5d48c
aminsato <amin.saradar@yahoo.com>
2024-07-24 11:39:18 +0330

    Update QrAddressScreen.kt

--- app/src/main/java/com/vultisig/wallet/presenter/qr_address/QrAddressScreen.kt
+++ app/src/main/java/com/vultisig/wallet/presenter/qr_address/QrAddressScreen.kt
@@ -1,22 +1,24 @@
 package com.vultisig.wallet.presenter.qr_address
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.PathEffect
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.vultisig.wallet.R
@@ -46,36 +48,37 @@ internal fun QrAddressScreen(navController: NavHostController) {
             )
         },
     ) {
-        Box(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
-                .consumeWindowInsets(it)
+                .padding(it)
                 .background(Theme.colors.oxfordBlue800),
-            Alignment.Center
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            val screenWidth = LocalConfiguration.current.screenWidthDp
-            val qrBoxSize = ((screenWidth * .8).coerceAtMost(300.0)).dp
-
             Text(
                 text = address,
-                style = Theme.menlo.body1,
-                modifier = Modifier
-                    .align(Alignment.TopCenter)
-                    .padding(top = 128.dp)
+                style = Theme.menlo.body1
             )
 
+            BoxWithConstraints(
+                Modifier.weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                val qrBoxSize = ((maxWidth.value * .8).coerceAtMost(300.0)).dp
+                val segment = with(LocalDensity.current) {
+                    qrBoxSize.div(5).toPx()
+                }
 
-            val segment = with(LocalDensity.current) {
-                qrBoxSize.div(5).toPx()
+                QRCodeKeyGenImage(
+                    address,
+                    pathEffect = PathEffect.dashPathEffect(floatArrayOf(segment, segment)),
+                    modifier = Modifier
+                        .width(min(maxHeight, maxWidth))
+                        .padding(all = 32.dp)
+                        .aspectRatio(1f),
+                )
             }
-
-            QRCodeKeyGenImage(
-                address,
-                pathEffect = PathEffect.dashPathEffect(floatArrayOf(segment, segment)),
-                modifier = Modifier
-                    .padding(all = 32.dp)
-                    .fillMaxWidth(),
-            )
         }
     }
 }
