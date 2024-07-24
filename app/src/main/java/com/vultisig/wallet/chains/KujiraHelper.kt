d9a78377880094cd4ac57aa36680bd8fff7351fd
Johnny Luo <johnnyluo1980@gmail.com>
2024-07-24 11:44:46 +1000

    bubble error

--- app/src/main/java/com/vultisig/wallet/chains/KujiraHelper.kt
+++ app/src/main/java/com/vultisig/wallet/chains/KujiraHelper.kt
@@ -119,6 +119,9 @@ internal class KujiraHelper(
         val hashes = wallet.core.jni.TransactionCompiler.preImageHashes(coinType, result)
         val preSigningOutput =
             wallet.core.jni.proto.TransactionCompiler.PreSigningOutput.parseFrom(hashes)
+        if (!preSigningOutput.errorMessage.isNullOrEmpty()) {
+            throw Exception(preSigningOutput.errorMessage)
+        }
         return listOf(Numeric.toHexStringNoPrefix(preSigningOutput.dataHash.toByteArray()))
     }
 
