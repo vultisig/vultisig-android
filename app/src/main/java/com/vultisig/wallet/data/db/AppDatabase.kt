4b771cabf0d236e2221aad6a6cd725f7ce13e116
Amin <32006742+aminsato@users.noreply.github.com>
2024-08-02 13:50:55 +0330

    Fix Gaia chain name (#659)
    * Fix gaia chain name
    
    * Fix gaia chain name

--- app/src/main/java/com/vultisig/wallet/data/db/AppDatabase.kt
+++ app/src/main/java/com/vultisig/wallet/data/db/AppDatabase.kt
@@ -28,7 +28,7 @@ import com.vultisig.wallet.data.db.models.VaultOrderEntity
         AddressBookEntryEntity::class,
         AddressBookOrderEntity::class,
     ],
-    version = 10,
+    version = 11,
     exportSchema = false,
 )
 internal abstract class AppDatabase : RoomDatabase() {
