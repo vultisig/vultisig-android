ade98a13d02ba9cdfccca6e95e4b129961a62039
yvebe <14962060+yvebe@users.noreply.github.com>
2024-08-14 16:39:54 +0400

    Update keygen device list layout

--- app/src/main/java/com/vultisig/wallet/presenter/keygen/DeviceList.kt
+++ app/src/main/java/com/vultisig/wallet/presenter/keygen/DeviceList.kt
@@ -1,64 +1,109 @@
 package com.vultisig.wallet.presenter.keygen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.vultisig.wallet.R
 import com.vultisig.wallet.common.Utils
-import com.vultisig.wallet.presenter.keygen.components.DeviceInfoItem
 import com.vultisig.wallet.ui.components.MultiColorButton
 import com.vultisig.wallet.ui.components.TopBar
+import com.vultisig.wallet.ui.components.UiSpacer
 import com.vultisig.wallet.ui.components.digitStringToWords
+import com.vultisig.wallet.ui.components.vultiGradient
 import com.vultisig.wallet.ui.theme.Theme
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 @Composable
-internal fun DeviceList(navController: NavHostController, viewModel: KeygenFlowViewModel) {
-    val textColor = Theme.colors.neutral0
+internal fun DeviceList(
+    navController: NavHostController,
+    viewModel: KeygenFlowViewModel
+) {
     val uiState by viewModel.uiState.collectAsState()
     val items = uiState.selection
+
+    DeviceList(
+        navController = navController,
+        localPartyId = viewModel.localPartyID,
+        items = items,
+        onContinueClick = {
+            viewModel.startKeygen()
+            viewModel.moveToState(KeygenFlowState.KEYGEN)
+        }
+    )
+}
+
+@Composable
+private fun DeviceList(
+    navController: NavController,
+    localPartyId: String,
+    items: List<String>,
+    onContinueClick: () -> Unit,
+) {
+    val textColor = Theme.colors.neutral0
+
     Scaffold(
-        modifier = Modifier
-            .background(Theme.colors.oxfordBlue800)
-            .padding(
-                vertical = 12.dp,
-                horizontal = 8.dp
-            ),
+        containerColor = Theme.colors.oxfordBlue800,
         bottomBar = {
-            MultiColorButton(
-                text = stringResource(R.string.device_list_screen_continue),
-                backgroundColor = Theme.colors.turquoise600Main,
-                textColor = Theme.colors.oxfordBlue600Main,
-                minHeight = 44.dp,
-                textStyle = Theme.montserrat.subtitle1,
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
                     .padding(
-                        start = 12.dp,
-                        end = 12.dp,
-                        bottom = 12.dp,
-                    )
+                        horizontal = 16.dp,
+                        vertical = 12.dp,
+                    ),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
             ) {
-                CoroutineScope(Dispatchers.IO).launch {
-                    viewModel.startKeygen()
-                    viewModel.moveToState(KeygenFlowState.KEYGEN)
-                }
+                Hint(
+                    text = digitStringToWords(
+                        R.string.device_list_desc1,
+                        Utils.getThreshold(items.count())
+                    )
+                )
+
+                Hint(
+                    text = if (items.count() < 3)
+                        stringResource(R.string.device_list_desc2)
+                    else stringResource(
+                        R.string.device_list_desc3
+                    )
+                )
+
+                MultiColorButton(
+                    text = stringResource(R.string.device_list_screen_continue),
+                    backgroundColor = Theme.colors.turquoise600Main,
+                    textColor = Theme.colors.oxfordBlue600Main,
+                    minHeight = 44.dp,
+                    textStyle = Theme.montserrat.subtitle1,
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    onClick = onContinueClick,
+                )
             }
         },
         topBar = {
@@ -69,12 +114,20 @@ internal fun DeviceList(navController: NavHostController, viewModel: KeygenFlowV
             )
         }
     ) {
-        LazyColumn(modifier = Modifier.padding(it)) {
+        LazyColumn(
+            modifier = Modifier
+                .padding(it)
+                .padding(
+                    horizontal = 16.dp,
+                )
+        ) {
             item {
                 Column(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalAlignment = CenterHorizontally,
                 ) {
+                    UiSpacer(size = 8.dp)
+
                     Text(
                         text = stringResource(
                             R.string.device_list_of_vault,
@@ -82,60 +135,110 @@ internal fun DeviceList(navController: NavHostController, viewModel: KeygenFlowV
                             items.count()
                         ),
                         color = textColor,
-                        style = Theme.montserrat.body1
+                        style = Theme.montserrat.subtitle2
                     )
-                    Spacer(modifier = Modifier.height(12.dp))
+
+                    UiSpacer(size = 12.dp)
 
                     Text(
                         text = stringResource(R.string.device_list_screen_with_these_devices),
                         color = textColor,
-                        style = Theme.montserrat.body2
+                        style = Theme.montserrat.body3
                     )
 
-                    Spacer(modifier = Modifier.height(12.dp))
+                    UiSpacer(size = 32.dp)
                 }
             }
+
             val thresholds = Utils.getThreshold(items.count())
-            items.forEachIndexed { index, item ->
-                item {
-                    if (item == viewModel.localPartyID) {
-                        DeviceInfoItem("${index + 1}. $item ${stringResource(R.string.this_device)}")
-                    } else {
-                        if (index < thresholds)
-                            DeviceInfoItem("${index + 1}. $item ${stringResource(R.string.pair_device)}")
-                        else
-                            DeviceInfoItem("${index + 1}. $item ${stringResource(R.string.backup_device)}")
-                    }
+            itemsIndexed(items) { index, item ->
+                if (item == localPartyId) {
+                    DeviceInfoItem("${index + 1}. $item ${stringResource(R.string.this_device)}")
+                } else {
+                    if (index < thresholds)
+                        DeviceInfoItem("${index + 1}. $item ${stringResource(R.string.pair_device)}")
+                    else
+                        DeviceInfoItem("${index + 1}. $item ${stringResource(R.string.backup_device)}")
                 }
+
+                UiSpacer(size = 16.dp)
             }
-            item {
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = CenterHorizontally,
-                ) {
-                    Spacer(modifier = Modifier.height(12.dp))
 
-                    Text(
-                        style = Theme.menlo.body2,
-                        text = digitStringToWords(
-                            R.string.device_list_desc1,
-                            Utils.getThreshold(items.count())
-                        ),
-                        color = textColor
-                    )
-                    Spacer(modifier = Modifier.height(10.dp))
-
-                    Text(
-                        style = Theme.menlo.body2,
-                        text = if (items.count() < 3) stringResource(R.string.device_list_desc2) else stringResource(
-                            R.string.device_list_desc3
-                        ),
-                        color = textColor
-                    )
-
-                    Spacer(modifier = Modifier.height(16.dp))
-                }
-            }
         }
     }
+}
+
+@Composable
+private fun DeviceInfoItem(
+    info: String,
+) {
+    val textColor = Theme.colors.neutral0
+
+    Text(
+        text = info,
+        color = textColor,
+        style = Theme.menlo.overline2,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = Theme.colors.oxfordBlue600Main,
+                shape = RoundedCornerShape(10.dp),
+            )
+            .padding(
+                vertical = 24.dp,
+                horizontal = 20.dp,
+            )
+    )
+}
+
+@Composable
+private fun Hint(
+    text: String,
+) {
+    val brushGradient = Brush.vultiGradient()
+
+    Row(
+        modifier = Modifier
+            .border(
+                width = 1.dp,
+                brush = brushGradient,
+                shape = RoundedCornerShape(10.dp)
+            )
+            .fillMaxWidth()
+            .padding(all = 16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Image(
+            painter = painterResource(id = android.R.drawable.ic_menu_info_details),
+            contentDescription = null,
+            modifier = Modifier
+                .graphicsLayer(alpha = 0.99f)
+                .drawWithCache {
+                    onDrawWithContent {
+                        drawContent()
+                        drawRect(brushGradient, blendMode = BlendMode.SrcAtop)
+                    }
+                }
+                .size(20.dp)
+        )
+
+        UiSpacer(size = 10.dp)
+
+        Text(
+            style = Theme.menlo.body1,
+            text = text,
+            color = Theme.colors.neutral0
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun DeviceListPreview() {
+    DeviceList(
+        navController = rememberNavController(),
+        localPartyId = "localPartyId",
+        items = listOf("device1", "device2", "device3"),
+        onContinueClick = {}
+    )
 }
---
+++ app/src/main/java/com/vultisig/wallet/ui/components/VultiGradient.kt
@@ -0,0 +1,13 @@
+package com.vultisig.wallet.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Brush
+import com.vultisig.wallet.ui.theme.Theme
+
+@Composable
+internal fun Brush.Companion.vultiGradient() = Brush.horizontalGradient(
+    colors = listOf(
+        Theme.colors.turquoise600Main,
+        Theme.colors.persianBlue600Main
+    )
+)
