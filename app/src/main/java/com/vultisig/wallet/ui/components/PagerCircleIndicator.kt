1bc54e9e5b48de27b6e62627cd1ad61695a15e31
yvebe <14962060+yvebe@users.noreply.github.com>
2024-08-16 16:20:38 +0400

    Update keygen UI

---
+++ app/src/main/java/com/vultisig/wallet/ui/components/PagerCircleIndicator.kt
@@ -0,0 +1,50 @@
+package com.vultisig.wallet.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.vultisig.wallet.ui.theme.Theme
+
+@Composable
+internal fun PagerCircleIndicator(
+    currentIndex: Int,
+    size: Int,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally)
+    ) {
+        repeat(size) { iteration ->
+            val color = if (currentIndex == iteration)
+                Theme.colors.turquoise800
+            else
+                Theme.colors.oxfordBlue200
+
+            Box(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .background(color)
+                    .size(8.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PagerCircleIndicatorPreview() {
+    PagerCircleIndicator(
+        currentIndex = 2,
+        size = 3
+    )
+}
