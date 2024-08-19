6827eea50cf7e18662c0f42ab46c580c64809575
aminsato <amin.saradar@yahoo.com>
2024-08-19 11:52:48 +0330

    Fix round issue

--- app/src/main/java/com/vultisig/wallet/ui/models/mappers/TokenValueToDecimalUiStringMapper.kt
+++ app/src/main/java/com/vultisig/wallet/ui/models/mappers/TokenValueToDecimalUiStringMapper.kt
@@ -2,9 +2,9 @@ package com.vultisig.wallet.ui.models.mappers
 
 import com.vultisig.wallet.data.mappers.MapperFunc
 import com.vultisig.wallet.data.models.TokenValue
-import java.math.BigDecimal
 import java.math.RoundingMode
-import java.text.NumberFormat
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
 import java.util.Locale
 import javax.inject.Inject
 
@@ -13,17 +13,16 @@ internal interface TokenValueToDecimalUiStringMapper : MapperFunc<TokenValue, St
 internal class TokenValueToDecimalUiStringMapperImpl @Inject constructor() :
     TokenValueToDecimalUiStringMapper {
 
-    private val numberFormat = NumberFormat.getInstance(Locale.getDefault())
+    private val decimalFormat = DecimalFormat(
+        "#,###.${"#".repeat(MAX_UI_TOKEN_VALUE_DECIMALS)}",
+        DecimalFormatSymbols(Locale.getDefault())
+    )
 
-    override fun invoke(from: TokenValue): String {
-        val decimal = from.decimal
+    override fun invoke(from: TokenValue): String = decimalFormat.format(
+        from.decimal
             .setScale(MAX_UI_TOKEN_VALUE_DECIMALS, RoundingMode.HALF_UP)
             .stripTrailingZeros()
-        // We need to handle values below 0.001 without using NumberFormat because it rounds them to 0
-        return if (decimal < BigDecimal.valueOf(0.001))
-            decimal.toPlainString()
-        else numberFormat.format(decimal)
-    }
+    )
 
     companion object {
         private const val MAX_UI_TOKEN_VALUE_DECIMALS = 8
