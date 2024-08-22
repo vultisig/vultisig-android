95fb6b9ab7cd9ad1aa48871cf917ea83729526d2
aminsato <amin.saradar@yahoo.com>
2024-08-22 16:14:30 +0330

    Update HomeViewModel.kt

--- app/src/main/java/com/vultisig/wallet/ui/models/HomeViewModel.kt
+++ app/src/main/java/com/vultisig/wallet/ui/models/HomeViewModel.kt
@@ -33,11 +33,9 @@ internal class HomeViewModel @Inject constructor(
 ) : ViewModel() {
 
     private var requestedVaultId: String? = savedStateHandle.remove(Destination.ARG_VAULT_ID)
-    private var showVaultList: Boolean =
-        savedStateHandle.remove(Destination.Home.ARG_SHOW_VAULT_LIST) ?: false
-    val uiState: MutableStateFlow<HomeUiModel>
-        get() = MutableStateFlow(showVaultList.let { HomeUiModel(showVaultList = it) })
-
+    private var showVaultList: String? =
+        savedStateHandle.remove(Destination.Home.ARG_SHOW_VAULT_LIST)
+    val uiState = MutableStateFlow(HomeUiModel(showVaultList = showVaultList.toBoolean()))
     init {
         collectLastOpenedVault()
     }
