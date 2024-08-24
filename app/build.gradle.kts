c1df525a2b085ba28f6818f197f7415e5a98772b
Johnny Luo <johnnyluo1980@gmail.com>
2024-08-24 13:50:56 +1000

    release v1.0.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.vultisig.wallet"
         minSdk = 26
         targetSdk = 34
-        versionCode = 15
-        versionName = "1.0"
+        versionCode = 17
+        versionName = "1.0.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
