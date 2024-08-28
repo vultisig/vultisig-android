74cf10b44ecc21911b3d3e1fa390802ad73810ed
yvebe <14962060+yvebe@users.noreply.github.com>
2024-08-28 20:27:52 +0400

    Disable minify in data module

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
