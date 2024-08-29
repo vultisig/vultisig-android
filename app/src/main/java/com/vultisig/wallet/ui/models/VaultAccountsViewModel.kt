b8f2cb115152956bc50142b839f66e2b910b1702
yevhen1sec <second.chance02.30@gmail.com>
2024-08-29 07:23:40 +0200

    Fix refresh balance indicator (#845)
    * ptr
    
    * refactor ptr
    
    * refactor ptr
    
    * refactor ptr
    
    * refactor ptr
    
    * refactor ptr

--- app/src/main/java/com/vultisig/wallet/ui/models/VaultAccountsViewModel.kt
+++ app/src/main/java/com/vultisig/wallet/ui/models/VaultAccountsViewModel.kt
@@ -18,6 +18,7 @@ import com.vultisig.wallet.ui.navigation.Destination
 import com.vultisig.wallet.ui.navigation.Navigator
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.first
@@ -87,7 +88,7 @@ internal class VaultAccountsViewModel @Inject constructor(
     fun refreshData() {
         val vaultId = vaultId ?: return
         updateRefreshing(true)
-        loadAccounts(vaultId)
+        loadAccounts(vaultId, true)
     }
 
     fun send() {
@@ -140,35 +141,44 @@ internal class VaultAccountsViewModel @Inject constructor(
         loadAccountsJob?.cancel()
     }
 
-    private fun loadAccounts(vaultId: String) {
+    private fun loadAccounts(vaultId: String, isRefresh: Boolean = false) {
         loadAccountsJob?.cancel()
         loadAccountsJob = viewModelScope.launch {
             accountsRepository
-                .loadAddresses(vaultId)
-                .map { it ->
-                    it.sortedBy {
-                        it.accounts.calculateAccountsTotalFiatValue()?.value?.unaryMinus()
-                    }
-                }
-                .catch {
-                    updateRefreshing(false)
-
-                    // TODO handle error
-                    Timber.e(it)
-                }.collect { accounts ->
-                    updateRefreshing(false)
-
-                    val totalFiatValue = accounts.calculateAddressesTotalFiatValue()
-                        ?.let(fiatValueToStringMapper::map)
-                    val accountsUiModel = accounts.map(addressToUiModelMapper::map)
-
-                    uiState.update {
-                        it.copy(
-                            totalFiatValue = totalFiatValue, accounts = accountsUiModel
-                        )
-                    }
-                }
+                .loadAddresses(vaultId, isRefresh)
+                .updateUiStateFromFlow()
         }
+    }
+
+    private suspend fun Flow<List<Address>>.updateUiStateFromFlow() =
+        this.map { it ->
+            it.sortByAccountsTotalFiatValue()
+        }
+            .catch {
+                updateRefreshing(false)
+
+                // TODO handle error
+                Timber.e(it)
+            }.collect { accounts ->
+                accounts.updateUiStateFromList()
+            }
+
+    private fun List<Address>.sortByAccountsTotalFiatValue() =
+        sortedBy {
+            it.accounts.calculateAccountsTotalFiatValue()?.value?.unaryMinus()
+        }
+
+    private fun List<Address>.updateUiStateFromList() {
+        val totalFiatValue = this.calculateAddressesTotalFiatValue()
+            ?.let(fiatValueToStringMapper::map)
+        val accountsUiModel = this.map(addressToUiModelMapper::map)
+
+        uiState.update {
+            it.copy(
+                totalFiatValue = totalFiatValue, accounts = accountsUiModel
+            )
+        }
+        updateRefreshing(false)
     }
 
     private fun updateRefreshing(isRefreshing: Boolean) {
