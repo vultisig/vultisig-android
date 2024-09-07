509b6828bb6d413da39a09ddf89b464a3424e589
aminsato <amin.saradar@yahoo.com>
2024-09-07 15:53:16 +0330

    Update TokenValueToDecimalUiStringMapper.kt
    formatDecimal(from.decimal)

--- app/src/main/java/com/vultisig/wallet/ui/models/mappers/TokenValueToDecimalUiStringMapper.kt
+++ app/src/main/java/com/vultisig/wallet/ui/models/mappers/TokenValueToDecimalUiStringMapper.kt
@@ -2,6 +2,7 @@ package com.vultisig.wallet.ui.models.mappers
 
 import com.vultisig.wallet.data.mappers.MapperFunc
 import com.vultisig.wallet.data.models.TokenValue
+import java.math.BigDecimal
 import java.math.RoundingMode
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
@@ -18,14 +19,44 @@ internal class TokenValueToDecimalUiStringMapperImpl @Inject constructor() :
         DecimalFormatSymbols(Locale.getDefault())
     )
 
-    override fun invoke(from: TokenValue): String = decimalFormat.format(
-        from.decimal
-            .setScale(MAX_UI_TOKEN_VALUE_DECIMALS, RoundingMode.HALF_UP)
-            .stripTrailingZeros()
-    )
+    override fun invoke(from: TokenValue): String {
+        try {
+            val decimal = from.decimal
+            val decimalValue = when {
+                decimal >= ONE_BILLION -> {
+                    String.format(
+                        "%sB",
+                        formatDecimal(decimal.divide(ONE_BILLION))
+                    )
+                }
+                decimal >= ONE_MILLION -> {
+                    String.format(
+                        "%sM",
+                        formatDecimal(decimal.divide(ONE_MILLION))
+                    )
+                }
+                else -> formatDecimal(decimal)
+            }
+            return decimalValue
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return BigDecimal.ZERO.toString()
+        }
+    }
+
 
     companion object {
         private const val MAX_UI_TOKEN_VALUE_DECIMALS = 8
+        private val ONE_BILLION = BigDecimal(1_000_000_000)
+        private val ONE_MILLION = BigDecimal(1_000_000)
     }
 
+    private fun formatDecimal(decimal: BigDecimal): String = decimalFormat.format(
+        decimal
+            .setScale(
+                MAX_UI_TOKEN_VALUE_DECIMALS,
+                RoundingMode.HALF_UP
+            )
+            .stripTrailingZeros()
+    )
 }
