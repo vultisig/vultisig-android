e57b605d69dfa3073e4701ef6b7c206458cb3ae1
Amin <32006742+aminsato@users.noreply.github.com>
2024-09-17 15:18:36 +0330

    Replace Gson with Json in lifi (#989)

--- app/src/main/java/com/vultisig/wallet/data/api/LiFiChainApi.kt
+++ app/src/main/java/com/vultisig/wallet/data/api/LiFiChainApi.kt
@@ -1,11 +1,10 @@
 package com.vultisig.wallet.data.api
 
-import com.google.gson.Gson
 import com.vultisig.wallet.data.api.models.LiFiSwapQuoteJson
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
-import io.ktor.client.statement.bodyAsText
 import javax.inject.Inject
 
 internal interface LiFiChainApi {
@@ -21,7 +20,6 @@ internal interface LiFiChainApi {
 }
 
 internal class LiFiChainApiImpl @Inject constructor(
-    private val gson: Gson,
     private val httpClient: HttpClient,
 ) : LiFiChainApi {
     override suspend fun getSwapQuote(
@@ -32,17 +30,14 @@ internal class LiFiChainApiImpl @Inject constructor(
         fromAmount: String,
         fromAddress: String,
         toAddress: String,
-    ): LiFiSwapQuoteJson {
-        val response = httpClient
-            .get("https://li.quest/v1/quote"){
-                parameter("fromChain", fromChain)
-                parameter("toChain", toChain)
-                parameter("fromToken", fromToken)
-                parameter("toToken", toToken)
-                parameter("fromAmount", fromAmount)
-                parameter("fromAddress", fromAddress)
-                parameter("toAddress", toAddress)
-            }
-        return gson.fromJson(response.bodyAsText(), LiFiSwapQuoteJson::class.java)
-    }
+    ): LiFiSwapQuoteJson = httpClient
+        .get("https://li.quest/v1/quote") {
+            parameter("fromChain", fromChain)
+            parameter("toChain", toChain)
+            parameter("fromToken", fromToken)
+            parameter("toToken", toToken)
+            parameter("fromAmount", fromAmount)
+            parameter("fromAddress", fromAddress)
+            parameter("toAddress", toAddress)
+        }.body()
 }
