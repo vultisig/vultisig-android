b992ec9b1460338cf09d5522a251a213118a1055
Amin <32006742+aminsato@users.noreply.github.com>
2024-09-23 11:09:48 +0330

    Replace Gson with Json in Old Vault (#1012)

--- app/src/main/java/com/vultisig/wallet/data/mappers/VaultFromOldJsonMapper.kt
+++ app/src/main/java/com/vultisig/wallet/data/mappers/VaultFromOldJsonMapper.kt
@@ -1,15 +1,17 @@
 package com.vultisig.wallet.data.mappers
 
-import com.google.gson.Gson
 import com.vultisig.wallet.data.models.KeyShare
 import com.vultisig.wallet.data.models.OldJsonVault
 import com.vultisig.wallet.data.models.Vault
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import java.util.UUID
 import javax.inject.Inject
 
 internal interface VaultFromOldJsonMapper : MapperFunc<OldJsonVault, Vault>
 
-internal class VaultFromOldJsonMapperImpl @Inject constructor(private val gson: Gson) :
+internal class VaultFromOldJsonMapperImpl @Inject constructor(private val json: Json) :
     VaultFromOldJsonMapper {
     override fun invoke(vault: OldJsonVault): Vault {
         return Vault(
@@ -20,13 +22,17 @@ internal class VaultFromOldJsonMapperImpl @Inject constructor(private val gson: 
             hexChainCode = vault.hexChainCode,
             localPartyID = vault.localPartyID,
             signers = vault.signers,
-            resharePrefix = vault.keyShares[0].keyShare.extractResharePrefix(),
-            keyshares = vault.keyShares.map { KeyShare(it.pubKey,it.keyShare) },
+            resharePrefix = json.decodeFromString<OldVaultReshare>(
+                vault.keyShares[0].keyShare
+            ).resharePrefix,
+            keyshares = vault.keyShares.map { KeyShare(it.pubKey, it.keyShare) },
             coins = emptyList(),
         )
     }
 
-    private fun String.extractResharePrefix(): String {
-        return gson.fromJson(this, HashMap::class.java)["reshare_prefix"].toString()
-    }
+    @Serializable
+    private data class OldVaultReshare(
+        @SerialName("reshare_prefix")
+        val resharePrefix: String,
+    )
 }
