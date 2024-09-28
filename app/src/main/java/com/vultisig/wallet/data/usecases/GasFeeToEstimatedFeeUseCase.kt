8acc2a09634816eb29dd3e3b3aaffede44edf713
aminsato <amin.saradar@yahoo.com>
2024-09-28 11:48:55 +0330

    Update GasFeeToEstimatedFeeUseCase.kt

--- app/src/main/java/com/vultisig/wallet/data/usecases/GasFeeToEstimatedFeeUseCase.kt
+++ app/src/main/java/com/vultisig/wallet/data/usecases/GasFeeToEstimatedFeeUseCase.kt
@@ -3,6 +3,7 @@ package com.vultisig.wallet.ui.models.mappers
 import com.vultisig.wallet.data.models.TokenValue
 import com.vultisig.wallet.data.models.GasFeeParams
 import com.vultisig.wallet.data.repositories.AppCurrencyRepository
+import com.vultisig.wallet.data.repositories.TokenRepository
 import com.vultisig.wallet.data.usecases.ConvertTokenValueToFiatUseCase
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
@@ -14,18 +15,21 @@ internal class GasFeeToEstimatedFeeUseCaseImpl @Inject constructor(
     private val convertTokenValueToFiat: ConvertTokenValueToFiatUseCase,
     private val fiatValueToStringMapper: FiatValueToStringMapper,
     private val appCurrencyRepository: AppCurrencyRepository,
+    private val tokenRepository: TokenRepository,
+
     ) : GasFeeToEstimatedFeeUseCase {
 
     override suspend fun invoke(from: GasFeeParams): String {
         val appCurrency = appCurrencyRepository.currency.first()
+        //
         val tokenValue = TokenValue(
             value = from.gasFee.value.multiply(from.gasLimit),
             unit = from.gasFee.unit,
             decimals = from.gasFee.decimals
         )
-
+        val nativeToken = tokenRepository.getNativeToken(from.selectedToken.chain.id)
         val fiatFees = convertTokenValueToFiat(
-            from.selectedToken,
+            nativeToken,
             tokenValue,
             appCurrency
         )
