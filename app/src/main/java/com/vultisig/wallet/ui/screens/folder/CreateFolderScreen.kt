589038603aaf3abf046fab788b00ea01595bd7e6
yevhen1sec <second.chance02.30@gmail.com>
2024-10-09 10:59:08 +0200

    code clean

--- app/src/main/java/com/vultisig/wallet/ui/screens/folder/CreateFolderScreen.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/folder/CreateFolderScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
