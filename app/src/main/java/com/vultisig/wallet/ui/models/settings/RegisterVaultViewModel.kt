688a4dda6ece9592971f22e4759683071bccdefd
aminsato <amin.saradar@yahoo.com>
2024-10-11 15:07:53 +0330

    Update Register Vault Screen

--- app/src/main/java/com/vultisig/wallet/ui/models/settings/RegisterVaultViewModel.kt
+++ app/src/main/java/com/vultisig/wallet/ui/models/settings/RegisterVaultViewModel.kt
@@ -1,24 +1,138 @@
 package com.vultisig.wallet.ui.models.settings
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.vultisig.wallet.R
+import com.vultisig.wallet.data.common.QRCODE_DIRECTORY_NAME_FULL
+import com.vultisig.wallet.data.common.saveBitmapToDownloads
+import com.vultisig.wallet.data.common.sha256
+import com.vultisig.wallet.data.repositories.VaultRepository
+import com.vultisig.wallet.data.usecases.GenerateQrBitmap
+import com.vultisig.wallet.ui.models.ShareVaultQrModel
 import com.vultisig.wallet.ui.navigation.Destination
-import com.vultisig.wallet.ui.navigation.Navigator
+import com.vultisig.wallet.ui.utils.SnackbarFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import javax.inject.Inject
+
+internal data class RegisterVaultUiModel(
+    val fileName: String = "",
+    val shareVaultQrString: String = "",
+    val bitmap: Bitmap? = null,
+)
 
 @HiltViewModel
 internal class RegisterVaultViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val navigator: Navigator<Destination>,
+    private val snackbarFlow: SnackbarFlow,
+    @ApplicationContext private val context: Context,
+    private val vaultRepository: VaultRepository,
+    private val json: Json,
+    private val generateQrBitmap: GenerateQrBitmap,
 ) : ViewModel() {
     val vaultId = requireNotNull(savedStateHandle.get<String>(Destination.ARG_VAULT_ID))
+    val permissionChannel = Channel<Boolean>()
+    val uiModel = MutableStateFlow(RegisterVaultUiModel())
+    private var hasWritePermission by mutableStateOf(
+        ContextCompat.checkSelfPermission(
+            context, Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+    )
 
-    fun navigateToShareVaultQrScreen() {
+    init {
         viewModelScope.launch {
-            navigator.navigate(Destination.ShareVaultQr(vaultId))
+            vaultRepository.get(vaultId)?.let { vault ->
+                val uid =
+                    ("${vault.name} - ${vault.pubKeyECDSA} - " + "${vault.pubKeyEDDSA} - ${vault.hexChainCode}").sha256()
+
+                val shareVaultQrModel = ShareVaultQrModel(
+                    name = vault.name,
+                    publicKeyEcdsa = vault.pubKeyECDSA,
+                    publicKeyEddsa = vault.pubKeyEDDSA,
+                    hexChainCode = vault.hexChainCode,
+                    uid = uid
+                )
+
+                uiModel.update {
+                    it.copy(
+                        shareVaultQrString = json.encodeToString(shareVaultQrModel)
+                    )
+                }
+
+                if (!hasWritePermission) {
+                    permissionChannel.send(true)
+                }
+            }
         }
+    }
+
+    internal fun onPermissionResult(isGranted: Boolean) {
+        if (!isGranted) {
+            viewModelScope.launch {
+                snackbarFlow.showMessage(
+                    context.getString(R.string.share_qr_screen_permission_required)
+                )
+            }
+        }
+    }
+
+    fun generateBitmap(
+        shareVaultQrString: String,
+        mainColor: Color,
+        backgroundColor: Color,
+        logo: Bitmap,
+    ) {
+        val bitmap = generateQrBitmap(
+            shareVaultQrString,
+            mainColor,
+            backgroundColor,
+            logo
+        )
+        uiModel.update {
+            it.copy(
+                bitmap = bitmap
+            )
+        }
+    }
+
+
+    fun saveBitmap() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val uri = context.saveBitmapToDownloads(
+                requireNotNull(uiModel.value.bitmap),
+                requireNotNull(uiModel.value.fileName)
+            )
+            uiModel.value.bitmap?.recycle()
+            if (uri != null) {
+                showSnackbarMessage()
+            }
+        }
+    }
+
+
+    private suspend fun showSnackbarMessage() {
+        snackbarFlow.showMessage(
+            context.getString(
+                R.string.vault_settings_success_backup_file,
+                "$QRCODE_DIRECTORY_NAME_FULL/${uiModel.value.fileName}"
+            )
+        )
     }
 }
--- app/src/main/java/com/vultisig/wallet/ui/screens/settings/RegisterVaultScreen.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/settings/RegisterVaultScreen.kt
@@ -1,18 +1,28 @@
 package com.vultisig.wallet.ui.screens.settings
 
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -28,10 +38,13 @@ import androidx.navigation.NavHostController
 import com.vultisig.wallet.R
 import com.vultisig.wallet.ui.components.TopBar
 import com.vultisig.wallet.ui.components.UiSpacer
+import com.vultisig.wallet.ui.components.clickOnce
 import com.vultisig.wallet.ui.components.library.form.FormCard
 import com.vultisig.wallet.ui.models.settings.RegisterVaultViewModel
 import com.vultisig.wallet.ui.theme.Theme.colors
 import com.vultisig.wallet.ui.theme.Theme.montserrat
+import com.vultisig.wallet.ui.utils.WriteFilePermissionHandler
+import kotlinx.coroutines.flow.receiveAsFlow
 
 @Composable
 internal fun RegisterVaultScreen(
@@ -39,6 +52,32 @@ internal fun RegisterVaultScreen(
     viewModel: RegisterVaultViewModel = hiltViewModel(),
 ) {
     val uriHandler = LocalUriHandler.current
+    val mainColor = colors.neutral0
+    val backgroundColor = colors.transparent
+    val context = LocalContext.current
+    val uiModel by viewModel.uiModel.collectAsState()
+
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+        WriteFilePermissionHandler(
+            viewModel.permissionChannel.receiveAsFlow(),
+            viewModel::onPermissionResult
+        )
+    }
+
+    LaunchedEffect(uiModel.shareVaultQrString) {
+        if (uiModel.shareVaultQrString.isNotEmpty()) {
+            viewModel.generateBitmap(
+                uiModel.shareVaultQrString,
+                mainColor,
+                backgroundColor,
+                BitmapFactory.decodeResource(
+                    context.resources,
+                    R.drawable.ic_qr_vultisig
+                )
+            )
+        }
+    }
+
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
@@ -48,15 +87,16 @@ internal fun RegisterVaultScreen(
                 navController = navController,
                 centerText = stringResource(R.string.register_vault_screen_title),
                 startIcon = R.drawable.caret_left,
-                endIcon = R.drawable.ic_share,
-                onEndIconClick = viewModel::navigateToShareVaultQrScreen,
             )
         }
     ) {
         Box(modifier = Modifier.padding(it)) {
-            RegisterVaultScreen {
-                uriHandler.openUri("https://web.vultisig.com/")
-            }
+            RegisterVaultScreen(
+                onVultisigLinkClick = {
+                    uriHandler.openUri("https://web.vultisig.com/")
+                },
+                onSaveVaultClick = viewModel::saveBitmap
+            )
         }
     }
 }
@@ -65,6 +105,7 @@ internal fun RegisterVaultScreen(
 @Composable
 private fun RegisterVaultScreen(
     onVultisigLinkClick: () -> Unit,
+    onSaveVaultClick: () -> Unit,
 ) {
     Column(
         modifier = Modifier
@@ -80,14 +121,40 @@ private fun RegisterVaultScreen(
             modifier = Modifier.fillMaxWidth(0.7f)
         )
         UiSpacer(size = 32.dp)
-        FormCard(modifier = Modifier.fillMaxWidth()) {
+        FormCard(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
             Column(modifier = Modifier.padding(16.dp)) {
-                Text(
-                    text = stringResource(R.string.register_vault_screen_step_1),
-                    color = colors.neutral0,
-                    style = montserrat.body2,
-                )
-                UiSpacer(size = 4.dp)
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Text(
+                        text = stringResource(R.string.register_vault_screen_step_1_number_1),
+                        color = colors.neutral0,
+                        style = montserrat.body2,
+                    )
+                    Text(
+                        text = stringResource(R.string.register_vault_screen_step_1_save),
+                        color = colors.oxfordBlue600Main,
+                        style = montserrat.subtitle2,
+                        modifier = Modifier
+                            .clip(shape = RoundedCornerShape(8.dp))
+                            .clickOnce(onClick = onSaveVaultClick)
+                            .background(color = colors.turquoise600Main)
+                            .padding(
+                                vertical = 4.dp,
+                                horizontal = 8.dp
+                            )
+                    )
+                    Text(
+                        text = stringResource(R.string.register_vault_screen_step_1_your_vault_qr),
+                        color = colors.neutral0,
+                        style = montserrat.body2,
+                    )
+                }
+                UiSpacer(size = 6.dp)
                 Text(
                     text = stringResource(R.string.register_vault_screen_step_1_top_right),
                     color = colors.neutral0,
@@ -149,6 +216,6 @@ private fun RegisterVaultScreen(
 @Preview
 @Composable
 private fun RegisterVaultScreenPreview() {
-    RegisterVaultScreen {}
+    RegisterVaultScreen({}, {})
 }
 
