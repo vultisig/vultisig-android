7249abcd640b17d5e429f168d629e773acacdf07
aminsato <amin.saradar@yahoo.com>
2024-10-22 13:57:51 +0330

    Init

--- app/src/main/java/com/vultisig/wallet/ui/screens/settings/RegisterVaultScreen.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/settings/RegisterVaultScreen.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -120,6 +122,7 @@ private fun RegisterVaultScreen(
     Column(
         modifier = Modifier
             .padding(16.dp)
+            .verticalScroll(rememberScrollState())
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
