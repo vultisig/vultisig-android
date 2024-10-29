1e45fadf5e31a3bf13d63fa9304df1f9d08f7273
yevhen1sec <second.chance02.30@gmail.com>
2024-10-29 10:20:00 +0100

    Add local password hint (#1239)
    * Add local password hint
    
    * code clean
    
    * code clean
    
    * password hint error message
    
    * code clean
    
    ---------
    
    Co-authored-by: yevhen <garret.evg@gmail.com>

--- data/src/main/kotlin/com/vultisig/wallet/data/repositories/VaultDataStoreRepository.kt
+++ data/src/main/kotlin/com/vultisig/wallet/data/repositories/VaultDataStoreRepository.kt
@@ -1,6 +1,7 @@
 package com.vultisig.wallet.data.repositories
 
 import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
 import com.vultisig.wallet.data.sources.AppDataStore
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -9,6 +10,10 @@ interface VaultDataStoreRepository {
     suspend fun setBackupStatus(vaultId: String, status: Boolean)
 
     suspend fun readBackupStatus(vaultId: String): Flow<Boolean>
+
+    suspend fun setBackupHint(vaultFileName: String, hint: String)
+
+    suspend fun readBackupHint(vaultFileName: String): Flow<String>
 }
 
 internal class VaultDataStoreRepositoryImpl @Inject constructor(
@@ -16,15 +21,25 @@ internal class VaultDataStoreRepositoryImpl @Inject constructor(
 ) : VaultDataStoreRepository {
     override suspend fun setBackupStatus(vaultId: String, status: Boolean) {
         appDataStore.editData { preferences ->
-            preferences[onVaultBackupKey(vaultId)] = status
+            preferences[onVaultBackupStatusKey(vaultId)] = status
         }
     }
 
     override suspend fun readBackupStatus(vaultId: String): Flow<Boolean> =
-        appDataStore.readData(onVaultBackupKey(vaultId), true)
+        appDataStore.readData(onVaultBackupStatusKey(vaultId), true)
+
+    override suspend fun setBackupHint(vaultFileName: String, hint: String) {
+        appDataStore.editData { preferences ->
+            preferences[onVaultBackupHintKey(vaultFileName)] = hint
+        }
+    }
+
+    override suspend fun readBackupHint(vaultFileName: String): Flow<String> =
+        appDataStore.readData(onVaultBackupHintKey(vaultFileName), "")
 
     private companion object PreferencesKey {
-        fun onVaultBackupKey(vaultId: String) = booleanPreferencesKey(name = "vault_backup/$vaultId")
+        fun onVaultBackupStatusKey(vaultId: String) = booleanPreferencesKey(name = "vault_backup/$vaultId")
+        fun onVaultBackupHintKey(vaultFileName: String) = stringPreferencesKey(name = "vault_backup_hint/$vaultFileName")
     }
 }
 
