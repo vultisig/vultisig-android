1fc17b29b512a39fa94380ec26d2ec7ce150e78d
Amin <32006742+aminsato@users.noreply.github.com>
2024-11-12 11:39:42 +0330

    Add verticalScroll modifier on the screens that are missing it (#1330)
    * Implement vertical scroll functionality on the screens that are missing it
    
    * Remove unnecessary codes

--- app/src/main/java/com/vultisig/wallet/ui/screens/ShareVaultQrScreen.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/ShareVaultQrScreen.kt
@@ -18,7 +18,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -204,6 +206,7 @@ internal fun ShareVaultQrScreen(
         Box(
             modifier = Modifier
                 .fillMaxSize()
+                .verticalScroll(rememberScrollState())
                 .padding(it),
             contentAlignment = Alignment.Center,
         ) {
