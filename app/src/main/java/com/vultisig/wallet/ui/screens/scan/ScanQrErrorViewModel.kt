9693f4bb6336a47769bdfc6bc6777c360d1e7b18
yevhen1sec <second.chance02.30@gmail.com>
2024-11-12 10:15:49 +0100

    Add scan error screen (#1343)
    * Add scan error screen
    
    * clean
    
    * clean
    
    * clean

--- app/src/main/java/com/vultisig/wallet/ui/screens/scan/ScanQrErrorViewModel.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/scan/ScanQrErrorViewModel.kt
@@ -1,10 +1,8 @@
 package com.vultisig.wallet.ui.screens.scan
 
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.vultisig.wallet.ui.navigation.Destination
-import com.vultisig.wallet.ui.navigation.NavigationOptions
 import com.vultisig.wallet.ui.navigation.Navigator
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -13,20 +11,10 @@ import javax.inject.Inject
 @HiltViewModel
 internal class ScanQrErrorViewModel @Inject constructor(
     private val navigator: Navigator<Destination>,
-    savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
-
-    private val vaultId: String =
-        requireNotNull(savedStateHandle.get<String>(Destination.ARG_VAULT_ID))
-
-    fun enableChain() {
+    fun back() {
         viewModelScope.launch {
-            navigator.navigate(
-                Destination.AddChainAccount(
-                    vaultId = vaultId
-                ),
-                opts = NavigationOptions(popUpTo = Destination.Home().route)
-            )
+            navigator.navigate(Destination.Back)
         }
     }
 }
