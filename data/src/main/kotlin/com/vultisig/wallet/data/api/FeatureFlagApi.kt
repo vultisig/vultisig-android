477f3347e3862a1af500edcfff3162cfee7cccf4
yvebe <14962060+yvebe@users.noreply.github.com>
2024-11-15 00:13:20 +0200

    Refactor TssMessagePuller & GeneratingKeyViewModel (#1360)
    * Refactor TSS message puller
    
    * Refactor GeneratingKeyViewModel

--- data/src/main/kotlin/com/vultisig/wallet/data/api/FeatureFlagApi.kt
+++ data/src/main/kotlin/com/vultisig/wallet/data/api/FeatureFlagApi.kt
@@ -4,9 +4,8 @@ package com.vultisig.wallet.data.api
 import com.vultisig.wallet.data.api.models.FeatureFlagJson
 import com.vultisig.wallet.data.api.utils.throwIfUnsuccessful
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.request.get
-import io.ktor.client.statement.bodyAsText
-import kotlinx.serialization.json.Json
 import javax.inject.Inject
 
 interface FeatureFlagApi {
@@ -16,9 +15,10 @@ interface FeatureFlagApi {
 internal class FeatureFlagApiImpl @Inject constructor(
     private val http: HttpClient,
 ) : FeatureFlagApi {
-    override suspend fun getFeatureFlag(): FeatureFlagJson {
-        val resp = http.get("https://api.vultisig.com/feature/release.json").throwIfUnsuccessful()
-        val responseText = resp.bodyAsText()
-        return Json.decodeFromString<FeatureFlagJson>(responseText)
-    }
+
+    override suspend fun getFeatureFlag(): FeatureFlagJson =
+        http.get("https://api.vultisig.com/feature/release.json")
+            .throwIfUnsuccessful()
+            .body<FeatureFlagJson>()
+
 }
