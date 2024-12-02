ea1b42c5483a85153c048707ec806c74da19fe62
yvebe <14962060+yvebe@users.noreply.github.com>
2024-12-02 11:49:18 +0200

    Disable fast & active vaults creation (#1426)
    * Disable fast & active vaults creation
    
    * Disable reshare with vultiserver
    
    * Update select vault type screen UI for disabled fast & active vaults

--- app/src/main/java/com/vultisig/wallet/ui/screens/keygen/SelectVaultTypeScreen.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/keygen/SelectVaultTypeScreen.kt
@@ -2,40 +2,37 @@
 package com.vultisig.wallet.ui.screens.keygen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.vultisig.wallet.R
-import com.vultisig.wallet.ui.components.GradientButton
 import com.vultisig.wallet.ui.components.MultiColorButton
 import com.vultisig.wallet.ui.components.UiBarContainer
 import com.vultisig.wallet.ui.components.UiSpacer
 import com.vultisig.wallet.ui.models.keygen.SelectVaultTypeUiModel
 import com.vultisig.wallet.ui.models.keygen.SelectVaultTypeViewModel
 import com.vultisig.wallet.ui.theme.Theme
-import com.vultisig.wallet.ui.utils.asString
 
 @Composable
 internal fun SelectVaultTypeScreen(
@@ -77,6 +74,7 @@ private fun SelectVaultTypeScreen(
         Column(
             horizontalAlignment = CenterHorizontally,
         ) {
+            /* fast&active vaults are temporarily disabled
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -94,11 +92,38 @@ private fun SelectVaultTypeScreen(
                     )
                 }
             }
+             */
 
-            val selected = state.types[state.selectedTypeIndex]
+            UiSpacer(8.dp)
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_shield),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(40.dp)
+                        .background(
+                            color = Theme.colors.oxfordBlue400,
+                            shape = RoundedCornerShape(8.dp),
+                        )
+                        .padding(all = 10.dp)
+                )
+
+                UiSpacer(8.dp)
+
+                Text(
+                    text = stringResource(R.string.select_vault_type_secure_vault_title),
+                    color = textColor,
+                    style = Theme.montserrat.subtitle1,
+                )
+            }
+
+            // val selected = state.types[state.selectedTypeIndex]
 
             Image(
-                painter = painterResource(id = selected.drawableResId),
+                painter = painterResource(id = R.drawable.vault_type_secure),
                 contentDescription = "devices",
                 contentScale = ContentScale.Fit,
                 modifier = Modifier
@@ -112,6 +137,7 @@ private fun SelectVaultTypeScreen(
 
             UiSpacer(size = 24.dp)
 
+            /* fast&active vaults are temporarily disabled
             Text(
                 text = selected.description.asString(),
                 color = textColor,
@@ -119,8 +145,21 @@ private fun SelectVaultTypeScreen(
                 textAlign = TextAlign.Center,
                 lineHeight = 24.sp,
             )
+            */
 
-            UiSpacer(size = 24.dp)
+            Column {
+                TextPoint(
+                    text = stringResource(R.string.select_vault_type_secure_vault_point_0)
+                )
+                TextPoint(
+                    text = stringResource(R.string.select_vault_type_secure_vault_point_1)
+                )
+                TextPoint(
+                    text = stringResource(R.string.select_vault_type_secure_vault_point_2)
+                )
+            }
+
+            UiSpacer(size = 40.dp)
 
             MultiColorButton(
                 text = stringResource(id = R.string.select_vault_type_start),
@@ -136,27 +175,51 @@ private fun SelectVaultTypeScreen(
                 onClick = onStartClick,
             )
 
-            if (selected.hasPair) {
-                UiSpacer(size = 12.dp)
+            // if (selected.hasPair) {
+            UiSpacer(size = 12.dp)
 
-                MultiColorButton(
-                    text = stringResource(R.string.select_vault_type_pair_action),
-                    backgroundColor = Theme.colors.oxfordBlue800,
-                    textColor = Theme.colors.turquoise800,
-                    iconColor = Theme.colors.oxfordBlue800,
-                    borderSize = 1.dp,
-                    textStyle = Theme.montserrat.subtitle1,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(
-                            horizontal = 16.dp,
-                        ),
-                    onClick = onPairClick,
-                )
-            }
+            MultiColorButton(
+                text = stringResource(R.string.select_vault_type_pair_action),
+                backgroundColor = Theme.colors.oxfordBlue800,
+                textColor = Theme.colors.turquoise800,
+                iconColor = Theme.colors.oxfordBlue800,
+                borderSize = 1.dp,
+                textStyle = Theme.montserrat.subtitle1,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        horizontal = 16.dp,
+                    ),
+                onClick = onPairClick,
+            )
+            //}
 
             UiSpacer(size = 12.dp)
         }
+    }
+}
+
+@Composable
+private fun TextPoint(
+    text: String,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.check),
+            contentDescription = null,
+            tint = Theme.colors.turquoise600Main,
+            modifier = Modifier.size(24.dp),
+        )
+
+        UiSpacer(12.dp)
+
+        Text(
+            text = text,
+            color = Theme.colors.neutral0,
+            style = Theme.montserrat.body1,
+        )
     }
 }
 
