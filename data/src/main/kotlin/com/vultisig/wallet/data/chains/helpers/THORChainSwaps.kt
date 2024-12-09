1a0a2644523dab587ca073b6a5c6f6190619dd7b
hailhydra <johnnyluo1980@gmail.com>
2024-12-10 08:09:38 +1100

    Revert "Add a max slippage of 0.5% (#1427)"
    This reverts commit bda29ba32a526763d06c75482a240e2c4dffc28e.

--- data/src/main/kotlin/com/vultisig/wallet/data/chains/helpers/THORChainSwaps.kt
+++ data/src/main/kotlin/com/vultisig/wallet/data/chains/helpers/THORChainSwaps.kt
@@ -22,7 +22,6 @@ class THORChainSwaps(
     companion object {
         const val AFFILIATE_FEE_ADDRESS = "va"
         const val AFFILIATE_FEE_RATE = "50" // 50 BP
-        const val TOLERANCE_BPS = "50"
     }
 
     private fun getPreSignedInputData(
