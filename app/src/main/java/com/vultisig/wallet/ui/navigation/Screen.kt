685c57e6c754d3f0f60c320988919b4969a8d726
yevhen1sec <second.chance02.30@gmail.com>
2024-12-19 10:50:01 +0100

    Add Blockhash error message (#1510)
    * Add Blockhash error message

--- app/src/main/java/com/vultisig/wallet/ui/navigation/Screen.kt
+++ app/src/main/java/com/vultisig/wallet/ui/navigation/Screen.kt
@@ -2,8 +2,6 @@ package com.vultisig.wallet.ui.navigation
 
 sealed class Screen(val route: String) {
 
-    data object SigningError : Screen(route = "signing_error")
-
     data object AddChainAccount : Screen(route = "vault_detail/{vault_id}/add_account") {
         const val ARG_VAULT_ID: String = "vault_id"
         fun createRoute(vaultId: String): String {
