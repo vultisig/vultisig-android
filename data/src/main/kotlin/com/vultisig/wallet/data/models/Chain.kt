a7b376c72d5f2d75d8632e5d17935017d45d930d
Amin <32006742+aminsato@users.noreply.github.com>
2025-01-12 12:25:08 +0330

    Enable swap for Cronos chain (#1569)
    * Update Swaps
    
    Update SwapQuoteRepository.kt
    
    * Enable Swap on Cronos

--- data/src/main/kotlin/com/vultisig/wallet/data/models/Chain.kt
+++ data/src/main/kotlin/com/vultisig/wallet/data/models/Chain.kt
@@ -128,7 +128,7 @@ val Chain.IsSwapSupported: Boolean
 
         Chain.Avalanche, Chain.Base, Chain.BscChain, Chain.Ethereum, Chain.Optimism, Chain.Polygon,
 
-        Chain.Arbitrum, Chain.Blast,
+        Chain.Arbitrum, Chain.Blast, Chain.CronosChain
     )
 
 val Chain.isDepositSupported: Boolean
@@ -155,7 +155,7 @@ fun Chain.oneInchChainId(): Int =
         Chain.Polygon -> 137
         Chain.Optimism -> 10
         Chain.BscChain -> 56
-        // Chain.CronosChain -> 25
+        Chain.CronosChain -> 25
         Chain.ZkSync -> 324
         else -> throw SwapException.SwapRouteNotAvailable("Chain $this is not supported by 1inch API")
     }
--- data/src/main/kotlin/com/vultisig/wallet/data/repositories/SwapQuoteRepository.kt
+++ data/src/main/kotlin/com/vultisig/wallet/data/repositories/SwapQuoteRepository.kt
@@ -199,8 +199,9 @@ internal class SwapQuoteRepositoryImpl @Inject constructor(
         val fromToken =
             srcToken.contractAddress.ifEmpty { srcToken.ticker }
 
-        val toToken =
-            dstToken.contractAddress.ifEmpty { dstToken.ticker }
+        val toToken = if (dstToken.ticker == "CRO") "0x0000000000000000000000000000000000000000"
+        else dstToken.contractAddress.ifEmpty { dstToken.ticker }
+
 
         val liFiQuoteResponse = liFiChainApi.getSwapQuote(
             fromChain = srcToken.chain.oneInchChainId().toString(),
@@ -394,9 +395,9 @@ internal class SwapQuoteRepositoryImpl @Inject constructor(
                 SwapProvider.MAYA
             ) else setOf(SwapProvider.LIFI)
 
-            Chain.Blast -> setOf(SwapProvider.LIFI)
+            Chain.Blast, Chain.CronosChain -> setOf(SwapProvider.LIFI)
 
-            Chain.Solana, Chain.Polkadot, Chain.Dydx, Chain.CronosChain, Chain.ZkSync, Chain.Sui,
+            Chain.Solana, Chain.Polkadot, Chain.Dydx, Chain.ZkSync, Chain.Sui,
             Chain.Ton, Chain.Osmosis, Chain.Terra, Chain.TerraClassic, Chain.Noble, Chain.Ripple -> emptySet()
         }
 
