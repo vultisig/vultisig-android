9038afdaa594e4cd57cc64959cd17f8142dfb7a0
Amin <32006742+aminsato@users.noreply.github.com>
2025-01-22 10:45:24 +0330

    Redesign Choose Vault Type Screen (#1616)
    Redesign select vault type screen

--- app/src/main/java/com/vultisig/wallet/ui/components/rive/RiveAnimation.kt
+++ app/src/main/java/com/vultisig/wallet/ui/components/rive/RiveAnimation.kt
@@ -15,6 +15,7 @@ fun RiveAnimation(
     stateMachineName: String? = null,
     alignment: Alignment = Alignment.CENTER,
     fit: Fit = Fit.CONTAIN,
+    autoPlay: Boolean = true,
     onInit: (RiveAnimationView) -> Unit = {}
 ) {
     AndroidView(
@@ -25,6 +26,7 @@ fun RiveAnimation(
                     resId = animation,
                     stateMachineName = stateMachineName,
                     alignment = alignment,
+                    autoplay = autoPlay,
                     fit = fit,
                 )
             }
