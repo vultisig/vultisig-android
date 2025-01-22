a6ad0049fd41fc25cee43422a29ca0bd06c7d440
yvebe <14962060+yvebe@users.noreply.github.com>
2025-01-22 17:28:29 +0400

    Update VsButton and VsTopAppBar typography styles

--- app/src/main/java/com/vultisig/wallet/ui/components/topbar/VsTopAppBar.kt
+++ app/src/main/java/com/vultisig/wallet/ui/components/topbar/VsTopAppBar.kt
@@ -2,7 +2,9 @@
 
 package com.vultisig.wallet.ui.components.topbar
 
+import android.annotation.SuppressLint
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
@@ -45,12 +47,59 @@ fun VsTopAppBar(
     @DrawableRes iconRight: Int? = null,
     onIconRightClick: (() -> Unit)? = null,
 ) {
+    VsTopAppBar(
+        title = title,
+        iconLeft = iconLeft,
+        onIconLeftClick = onIconLeftClick,
+        actions = {
+            if (iconRight != null) {
+                VsTopAppBarAction(
+                    icon = iconRight,
+                    onClick = onIconRightClick,
+                )
+            }
+        },
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun VsTopAppBar(
+    modifier: Modifier = Modifier,
+    title: String? = null,
+    @DrawableRes iconLeft: Int? = null,
+    onIconLeftClick: (() -> Unit)? = null,
+    @SuppressLint("ComposableLambdaParameterNaming")
+    actions: @Composable RowScope.() -> Unit,
+) {
+    VsTopAppBar(
+        title = title,
+        navigationContent = {
+            if (iconLeft != null) {
+                VsTopAppBarAction(
+                    icon = iconLeft,
+                    onClick = onIconLeftClick,
+                )
+            }
+        },
+        actions = actions,
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun VsTopAppBar(
+    modifier: Modifier = Modifier,
+    title: String? = null,
+    navigationContent: @Composable () -> Unit,
+    actions: @Composable RowScope.() -> Unit,
+) {
     CenterAlignedTopAppBar(
         title = {
             if (title != null) {
                 Text(
                     text = title,
-                    style = Theme.montserrat.heading5, // TODO update typography
+                    style = Theme.brockmann.body.l.medium,
                     color = Theme.colors.text.primary,
                     textAlign = TextAlign.Center,
                 )
@@ -59,40 +108,29 @@ fun VsTopAppBar(
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = Theme.colors.backgrounds.primary,
         ),
-        navigationIcon = {
-            if (iconLeft != null) {
-                IconButton(
-                    onClick = clickOnce {
-                        onIconLeftClick?.invoke()
-                    }
-                ) {
-                    UiIcon(
-                        drawableResId = iconLeft,
-                        contentDescription = null,
-                        tint = Theme.colors.text.primary,
-                        size = 24.dp,
-                    )
-                }
-            }
-        },
-        actions = {
-            if (iconRight != null) {
-                IconButton(
-                    onClick = clickOnce {
-                        onIconRightClick?.invoke()
-                    }
-                ) {
-                    UiIcon(
-                        drawableResId = iconRight,
-                        contentDescription = null,
-                        tint = Theme.colors.text.primary,
-                        size = 24.dp,
-                    )
-                }
-            }
-        },
+        navigationIcon = navigationContent,
+        actions = actions,
         modifier = modifier,
     )
+}
+
+@Composable
+fun VsTopAppBarAction(
+    @DrawableRes icon: Int,
+    onClick: (() -> Unit)?,
+) {
+    IconButton(
+        onClick = clickOnce {
+            onClick?.invoke()
+        }
+    ) {
+        UiIcon(
+            drawableResId = icon,
+            contentDescription = null,
+            tint = Theme.colors.text.primary,
+            size = 24.dp,
+        )
+    }
 }
 
 @Preview
