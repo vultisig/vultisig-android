ae4691304b5646339132796e3197f6aa34da3e77
yevhen1sec <second.chance02.30@gmail.com>
2025-01-29 13:57:22 +0100

    Update onboarding (#1629)
    * Add RiveAnimation and all new onboarding animations (#1614)
    
    * Add button from design system
    
    * Fix modifier placement
    
    * Add new colors
    
    * Add alerts colors
    
    * Add neutrals colors
    
    * Add new top app bar (#1615)
    
    * Redesign Choose Vault Type Screen (#1616)
    
    Redesign select vault type screen
    
    * Revert "Redesign Choose Vault Type Screen (#1616)" (#1619)
    
    This reverts commit 9038afdaa594e4cd57cc64959cd17f8142dfb7a0.
    
    * Add Brockmann font (#1618)
    
    * Added Brockmann Font
    
    * Refactored Brockmann Font
    
    * Split typography into styles
    
    * Fix line height style for typography
    
    * Update VsButton and VsTopAppBar typography styles
    
    * Add peer discovery screen (#1623)
    
    * Add VsIconButton (#1626)
    
    * Add vsIconButton
    
    * UnusedResources change to warning
    
    * code clean
    
    * Fix buttons
    
    * onboarding init
    
    * bump rive version
    
    * Onboarding in progress
    
    * Onboarding in progress
    
    * Onboarding in progress RiveAnimation
    
    * Onboarding in progress animations are working
    
    * fixed animations
    
    * onboarding in progress
    
    * Added text
    
    * Added text animations
    
    * fix dashed progress indicator
    
    * restore navigation
    
    * code clean
    
    * refactor
    
    ---------
    
    Co-authored-by: yvebe <14962060+yvebe@users.noreply.github.com>
    Co-authored-by: Amin <32006742+aminsato@users.noreply.github.com>
    Co-authored-by: yevhen <garret.evg@gmail.com>

--- app/src/main/java/com/vultisig/wallet/ui/theme/Colors.kt
+++ app/src/main/java/com/vultisig/wallet/ui/theme/Colors.kt
@@ -1,5 +1,6 @@
 package com.vultisig.wallet.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 internal data class Colors(
@@ -63,6 +64,7 @@ internal data class Colors(
     val text: TextColors = TextColors(),
     val borders: BordersColors = BordersColors(),
     val alerts: AlertsColors = AlertsColors(),
+    val gradients: Gradients = Gradients(),
     val neutrals: NeutralsColors = NeutralsColors.Default
 
 
@@ -115,6 +117,15 @@ internal data class AlertsColors(
     val error: Color = Color(0xFFFF5C5C),
     val warning: Color = Color(0xFFFFC25C),
     val info: Color = Color(0xFF5CA7FF),
+)
+
+internal data class Gradients(
+    val primary: Brush = Brush.linearGradient(
+        colors = listOf(
+            Color(0xFF33E6BF),
+            Color(0xFF0439C7),
+        )
+    ),
 )
 
 internal data class NeutralsColors(
