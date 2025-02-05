a7d28fb5aa5808e82c9f0a4c13af92fa7b5776a3
Amin <32006742+aminsato@users.noreply.github.com>
2025-02-05 11:16:30 +0330

    Optimize Tron balance retrieval (#1657)

--- data/src/main/kotlin/com/vultisig/wallet/data/api/TronApi.kt
+++ data/src/main/kotlin/com/vultisig/wallet/data/api/TronApi.kt
@@ -3,26 +3,21 @@ package com.vultisig.wallet.data.api
 import com.vultisig.wallet.data.api.models.TronBalanceResponseJson
 import com.vultisig.wallet.data.api.models.TronBroadcastTxResponseJson
 import com.vultisig.wallet.data.api.models.TronSpecificBlockJson
-import com.vultisig.wallet.data.api.models.TronTRC20BalanceResponseJson
 import com.vultisig.wallet.data.api.models.TronTriggerConstantContractJson
-import com.vultisig.wallet.data.api.utils.postRpc
 import com.vultisig.wallet.data.common.stripHexPrefix
 import com.vultisig.wallet.data.models.Coin
-import com.vultisig.wallet.data.utils.Numeric
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.accept
+import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.http.path
-import kotlinx.serialization.json.add
-import kotlinx.serialization.json.buildJsonArray
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
 import timber.log.Timber
-import wallet.core.jni.Base58
 import java.math.BigInteger
 import javax.inject.Inject
 import kotlin.math.max
@@ -115,57 +110,18 @@ internal class TronApiImpl @Inject constructor(
 
     override suspend fun getBalance(coin: Coin): BigInteger {
         try {
+            val response = httpClient.get("$tronGrid/v1/accounts/${coin.address}")
+            val content = response.body<TronBalanceResponseJson>()
             return if (coin.isNativeToken)
-                getTronBalance(coin.address)
-            else getTRC20Balance(coin.address, coin.contractAddress)
+                content.tronBalanceResponseData.get(0).balance
+            else
+                content.tronBalanceResponseData.get(0).trc20.get(0).get(coin.contractAddress)
+                    ?: BigInteger.ZERO
         } catch (e: Exception) {
             Timber.e(e, "error getting tron balance")
             return BigInteger.ZERO
         }
     }
-
-
-    private suspend fun getTRC20Balance(address: String, contractAddress: String): BigInteger {
-        val walletAddress = Numeric.toHexString(Base58.decode(address))
-        val hexContractAddress = Numeric.toHexString(Base58.decode(contractAddress))
-        val paddedWalletAddress = "0".repeat(22) + walletAddress.drop(2)
-        val data = "0x70a08231$paddedWalletAddress"
-        val fromAddress = "0x${walletAddress.drop(4)}"
-        val toAddress = "0x${hexContractAddress.drop(4)}"
-        val params = buildJsonArray {
-            add(
-                buildJsonObject {
-                    put("from", fromAddress)
-                    put("to", toAddress)
-                    put("gas", "0x0")
-                    put("gasPrice", "0x0")
-                    put("value", "0x0")
-                    put("data", data)
-                }
-            )
-            add("latest")
-        }
-
-        val responseJson = httpClient.postRpc<TronTRC20BalanceResponseJson>(
-            url = "$tronGrid/jsonrpc",
-            method = "eth_call",
-            params = params
-        )
-        return BigInteger(responseJson.result.stripHexPrefix(), 16)
-    }
-
-    private suspend fun getTronBalance(address: String) = httpClient.post(rpcUrl) {
-        url {
-            path("wallet", "getaccount")
-        }
-        setBody(
-            buildJsonObject {
-                put("address", address)
-                put("visible", true)
-            }
-        )
-    }.body<TronBalanceResponseJson>().balance
-
 
     companion object {
         private const val FUNCTION_SELECTOR = "transfer(address,uint256)"
