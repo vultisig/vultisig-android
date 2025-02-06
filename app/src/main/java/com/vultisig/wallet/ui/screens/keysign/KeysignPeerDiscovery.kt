f0fa8c0286fbf0c9ea4bf84fd983d00d3c71426c
aminsato <amin.saradar@yahoo.com>
2025-02-06 12:53:57 +0330

    Update dependencies

--- app/src/main/java/com/vultisig/wallet/ui/screens/keysign/KeysignPeerDiscovery.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/keysign/KeysignPeerDiscovery.kt
@@ -2,6 +2,7 @@ package com.vultisig.wallet.ui.screens.keysign
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -49,7 +50,7 @@ internal fun KeysignPeerDiscovery(
     val participants = viewModel.participants.asFlow().collectAsState(initial = emptyList()).value
     val isLoading = viewModel.isLoading.collectAsState().value
     val context = LocalContext.current.applicationContext
-    val sharedViewModel: KeysignShareViewModel = hiltViewModel(LocalContext.current as MainActivity)
+    val sharedViewModel: KeysignShareViewModel = hiltViewModel(LocalActivity.current as MainActivity)
     val vault = sharedViewModel.vault ?: return
     val keysignPayload = sharedViewModel.keysignPayload
     val customMessagePayload = sharedViewModel.customMessagePayload
