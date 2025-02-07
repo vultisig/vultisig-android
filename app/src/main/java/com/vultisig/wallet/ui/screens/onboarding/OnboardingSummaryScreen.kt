c72689d19fad9582177d059a32c050259e2bb651
yevhen1sec <second.chance02.30@gmail.com>
2025-02-07 15:46:23 +0100

    update onboarding + summary

--- app/src/main/java/com/vultisig/wallet/ui/screens/onboarding/OnboardingSummaryScreen.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/onboarding/OnboardingSummaryScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,27 +16,40 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.vultisig.wallet.R
 import com.vultisig.wallet.ui.components.UiSpacer
 import com.vultisig.wallet.ui.components.buttons.VsButton
+import com.vultisig.wallet.ui.components.rive.RiveAnimation
 import com.vultisig.wallet.ui.models.onboarding.OnboardingSummaryViewModel
 import com.vultisig.wallet.ui.theme.Theme
 
 @Composable
-internal fun OnboardingSummaryScreen( //TODO: Add rive animation and checkbox
+internal fun OnboardingSummaryScreen(
     viewModel: OnboardingSummaryViewModel = hiltViewModel(),
 ) {
-    Box(
+    Column(
         modifier = Modifier
             .background(Theme.colors.backgrounds.primary)
             .fillMaxSize(),
     ) {
-        Column (Modifier.align(Alignment.BottomCenter)) {
-            VsButton(
-                onClick = viewModel::createVault,
-                label = stringResource(id = R.string.onboarding_summary_button),
+        Box(
+            modifier = Modifier
+                .background(Theme.colors.backgrounds.primary)
+                .weight(1f)
+                .fillMaxSize(),
+        ) {
+            RiveAnimation(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
+                    .align(Alignment.Center),
+                animation = R.raw.quick_summary,
             )
-            UiSpacer(32.dp)
-        }
+
+
+            }
+        VsButton(
+            onClick = viewModel::createVault,
+            label = stringResource(id = R.string.onboarding_summary_button),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        )
+        UiSpacer(32.dp)
     }
 }
