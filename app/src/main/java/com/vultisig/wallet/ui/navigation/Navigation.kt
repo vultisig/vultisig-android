5f33af00539abdb8c332e747a8aa79f4969e995a
aminsato <amin.saradar@yahoo.com>
2025-02-07 21:38:32 +0330

    Add Fast vault verify backup screen

--- app/src/main/java/com/vultisig/wallet/ui/navigation/Navigation.kt
+++ app/src/main/java/com/vultisig/wallet/ui/navigation/Navigation.kt
@@ -475,6 +475,11 @@ internal sealed class Destination(
 internal sealed class Route {
 
     @Serializable
+    data class VaultBackup(
+        val email: String,
+    ) : Route()
+
+    @Serializable
     data object Secret : Route()
 
     object VaultInfo {
