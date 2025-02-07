93295f344bf98df4a6703b7f9233438a96eb910c
yevhen1sec <second.chance02.30@gmail.com>
2025-02-07 21:18:36 +0100

    summary finished

---
+++ app/src/main/java/com/vultisig/wallet/ui/components/library/form/VsUiCheckbox.kt
@@ -0,0 +1,66 @@
+package com.vultisig.wallet.ui.components.library.form
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.vultisig.wallet.R
+import com.vultisig.wallet.ui.theme.Theme
+
+@Composable
+internal fun VsUiCheckbox(
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+) {
+    Surface(
+        color = if (checked) {
+            Theme.colors.backgrounds.success
+        } else {
+            Theme.colors.backgrounds.secondary
+        },
+        shape = CircleShape,
+        border = BorderStroke(1.dp,
+            if (checked) {
+                Theme.colors.alerts.success
+            } else {
+                Theme.colors.borders.normal
+            }
+        ),
+        modifier = Modifier
+            .size(24.dp)
+            .clickable(onClick = { onCheckedChange(!checked) })
+    ) {
+        if (checked) {
+            Icon(
+                modifier = Modifier.size(16.dp),
+                painter = painterResource(id = R.drawable.ic_check),
+                contentDescription = null,
+                tint = Theme.colors.alerts.success,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun VsUiCheckboxPreview() {
+    Row {
+        VsUiCheckbox(
+            checked = true,
+            onCheckedChange = {}
+        )
+
+        VsUiCheckbox(
+            checked = false,
+            onCheckedChange = {}
+        )
+    }
+}
