7941ab4a813d96c7cb2cd8c6ee55dd5428cc9f0d
yevhen1sec <second.chance02.30@gmail.com>
2025-02-08 10:00:52 +0100

    Add onboarding summary (#1671)
    * update onboarding + summary
    
    * summary finished
    
    * summary moved to other component

--- app/src/main/java/com/vultisig/wallet/ui/models/onboarding/OnboardingSummaryViewModel.kt
+++ app/src/main/java/com/vultisig/wallet/ui/models/onboarding/OnboardingSummaryViewModel.kt
@@ -7,6 +7,7 @@ import com.vultisig.wallet.ui.navigation.Destination
 import com.vultisig.wallet.ui.navigation.NavigationOptions
 import com.vultisig.wallet.ui.navigation.Navigator
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,7 +17,14 @@ internal class OnboardingSummaryViewModel @Inject constructor(
     private val navigator: Navigator<Destination>
 ) : ViewModel() {
 
+    val checkState = MutableStateFlow(false)
+
+    fun toggleCheck(checked: Boolean) = viewModelScope.launch {
+        checkState.value = checked
+    }
+
     fun createVault() = viewModelScope.launch {
+        if (!checkState.value) return@launch
         onboardingRepository.saveOnboardingState(true)
         navigator.navigate(
             dst = Destination.SelectVaultType,
