1b1b1a376f60801856ef711d8ac4546a49c86db5
aminsato <amin.saradar@yahoo.com>
2025-03-09 12:40:21 +0330

    Add Fuzion token

--- app/src/main/java/com/vultisig/wallet/data/models/CoinLogo.kt
+++ app/src/main/java/com/vultisig/wallet/data/models/CoinLogo.kt
@@ -95,6 +95,7 @@ internal fun Coins.getCoinLogo(logoName: String): ImageModel {
         "rkuji" -> R.drawable.rkuji
         "tron" -> R.drawable.tron
         "lvn" -> R.drawable.lvn
+        "fuzion" -> R.drawable.fuzion
         else -> logoName
     }
 }
