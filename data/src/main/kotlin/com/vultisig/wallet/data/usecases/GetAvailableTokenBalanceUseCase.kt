16b8f0ec1067f9b0b8b06dcb2fb32e578cdb4f40
Amin <32006742+aminsato@users.noreply.github.com>
2025-03-11 13:50:38 +0330

    Fix send max SOL (#1775)

--- data/src/main/kotlin/com/vultisig/wallet/data/usecases/GetAvailableTokenBalanceUseCase.kt
+++ data/src/main/kotlin/com/vultisig/wallet/data/usecases/GetAvailableTokenBalanceUseCase.kt
@@ -1,17 +1,14 @@
 package com.vultisig.wallet.data.usecases
 
-import com.vultisig.wallet.data.api.SolanaApi
 import com.vultisig.wallet.data.models.Account
-import com.vultisig.wallet.data.models.Chain
 import com.vultisig.wallet.data.models.TokenValue
 import java.math.BigInteger
 import javax.inject.Inject
 
 interface GetAvailableTokenBalanceUseCase : suspend (Account, BigInteger) -> TokenValue?
 
-internal class GetAvailableTokenBalanceUseCaseImpl @Inject constructor(
-    private val solanaApi: SolanaApi,
-) : GetAvailableTokenBalanceUseCase {
+internal class GetAvailableTokenBalanceUseCaseImpl @Inject constructor() :
+    GetAvailableTokenBalanceUseCase {
 
     override suspend fun invoke(
         account: Account,
@@ -19,13 +16,9 @@ internal class GetAvailableTokenBalanceUseCaseImpl @Inject constructor(
     ): TokenValue? {
         val token = account.token
         val tokenValue = account.tokenValue
-        val getModifiedToken = when (token.chain) {
-            Chain.Solana -> SolanaBalanceModifier(solanaApi)
-            else -> DefaultBalanceModifier()
-        }
         return if (token.isNativeToken) {
             tokenValue?.copy(
-                value = getModifiedToken(tokenValue).value.minus(gasCost)
+                value = tokenValue.value.minus(gasCost)
                     .coerceAtLeast(BigInteger.ZERO),
             )
         } else {
@@ -33,25 +26,4 @@ internal class GetAvailableTokenBalanceUseCaseImpl @Inject constructor(
         }
     }
 
-}
-
-private interface TokenBalanceModifier : suspend (TokenValue) -> TokenValue
-
-private class DefaultBalanceModifier : TokenBalanceModifier {
-    override suspend fun invoke(tokenValue: TokenValue) = tokenValue
-}
-
-private class SolanaBalanceModifier(
-    private val solanaApi: SolanaApi,
-) : TokenBalanceModifier {
-
-    override suspend fun invoke(tokenValue: TokenValue): TokenValue {
-        val availableTokenValue = calculateAvailableValue(tokenValue)
-        return tokenValue.copy(value = availableTokenValue)
-    }
-
-    private suspend fun calculateAvailableValue(tokenValue: TokenValue): BigInteger {
-        val rentExemption = solanaApi.getMinimumBalanceForRentExemption()
-        return (tokenValue.value - rentExemption).coerceAtLeast(BigInteger.ZERO)
-    }
 }
