88fc26423b5ada378327a2d61b353dc06c639e26
yvebe <14962060+yvebe@users.noreply.github.com>
2025-04-29 23:23:27 +0200

    Fix functions (#1907)
    * Fix functions
    
    * Remove duplicated kuji

--- app/src/main/java/com/vultisig/wallet/ui/screens/function/SwitchFunctionScreen.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/function/SwitchFunctionScreen.kt
@@ -13,6 +13,7 @@ import com.vultisig.wallet.ui.components.library.form.FormSelection
 import com.vultisig.wallet.ui.components.library.form.FormTextFieldCard
 import com.vultisig.wallet.ui.models.deposit.TokenMergeInfo
 import com.vultisig.wallet.ui.utils.UiText
+import com.vultisig.wallet.ui.utils.asString
 
 @Composable
 internal fun SwitchFunctionScreen(
@@ -38,7 +39,7 @@ internal fun SwitchFunctionScreen(
     FormSelection(
         selected = selectedToken,
         options = coinList,
-        mapTypeToString = { it.ticker.uppercase() },
+        mapTypeToString = { it.ticker },
         onSelectOption = onSelectCoin,
     )
 
@@ -89,7 +90,10 @@ internal fun SwitchFunctionScreen(
     }
 
     FormTextFieldCard(
-        title = stringResource(R.string.deposit_form_amount_title_without_balance),
+        title = stringResource(
+            R.string.deposit_form_amount_title,
+            balance.asString()
+        ),
         hint = stringResource(R.string.send_amount_currency_hint),
         keyboardType = KeyboardType.Number,
         textFieldState = amountFieldState,
