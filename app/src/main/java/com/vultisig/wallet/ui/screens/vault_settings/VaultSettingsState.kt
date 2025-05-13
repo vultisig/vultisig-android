2644624b9470bb73b8931ccae1cad2baa0ce9a3d
yvebe <14962060+yvebe@users.noreply.github.com>
2025-05-13 18:59:10 +0200

    Fix fast vault password inputs Make secure text field clickable area same as it's container Make password text fields scrollable for fast vault Hide reshare option when opening vault settings when we don't know if we'll have it Update Jetpack Compose

--- app/src/main/java/com/vultisig/wallet/ui/screens/vault_settings/VaultSettingsState.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/vault_settings/VaultSettingsState.kt
@@ -1,6 +1,7 @@
 package com.vultisig.wallet.ui.screens.vault_settings
 
 internal data class VaultSettingsState(
+    val hasReshare: Boolean = false,
     val hasFastSign: Boolean = false,
     val hasMigration: Boolean = false,
 )
