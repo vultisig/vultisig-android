8ee7904f2ff0551851ed56b80f6442118d4f01e0
JaimeToca <jaime.toca.munoz@gmail.com>
2025-07-05 02:41:09 +0700

    extract error

--- data/src/main/kotlin/com/vultisig/wallet/data/utils/RpcExtensions.kt
+++ data/src/main/kotlin/com/vultisig/wallet/data/utils/RpcExtensions.kt
@@ -8,22 +8,33 @@ import io.ktor.serialization.ContentConvertException
 import io.ktor.serialization.JsonConvertException
 import io.ktor.serialization.WebsocketContentConvertException
 import io.ktor.serialization.WebsocketDeserializeException
+import org.json.JSONObject
 
-suspend inline fun <reified T> HttpResponse.bodyOrThrow(): T {
+suspend inline fun <reified T> HttpResponse.bodyOrThrow(errorKey: String = "message"): T {
     return if (status.isSuccess()) {
         try {
             body()
         } catch (t: JsonConvertException) {
-            throw NetworkException(status.value, bodyAsText(), t)
+            throw NetworkException(status.value, extractError(this, errorKey), t)
         } catch (t: ContentConvertException) {
-            throw NetworkException(status.value, bodyAsText(), t)
+            throw NetworkException(status.value, extractError(this, errorKey), t)
         } catch (t: WebsocketDeserializeException) {
-            throw NetworkException(status.value, bodyAsText(), t)
+            throw NetworkException(status.value, extractError(this, errorKey), t)
         } catch (t: WebsocketContentConvertException) {
-            throw NetworkException(status.value, bodyAsText(), t)
+            throw NetworkException(status.value, extractError(this, errorKey), t)
         }
     } else {
         throw NetworkException(status.value, bodyAsText())
+    }
+}
+
+suspend fun extractError(response: HttpResponse, errorKey: String): String {
+    return try {
+        val responseBody = response.bodyAsText()
+        val json = JSONObject(responseBody)
+        return json.optString(errorKey, responseBody)
+    } catch (t: Throwable) {
+        response.bodyAsText()
     }
 }
 
