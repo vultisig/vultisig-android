f03d2243da780d3165eaa93845786baf7d3b0e73
Amin <32006742+aminsato@users.noreply.github.com>
2025-07-24 14:39:31 +0330

    Improve ETH swapping logic (#2221)
    * Improve ETH swapping logic
    
    * Refactor

--- app/src/main/java/com/vultisig/wallet/ui/models/mappers/SwapTransactionToUiModelMapper.kt
+++ app/src/main/java/com/vultisig/wallet/ui/models/mappers/SwapTransactionToUiModelMapper.kt
@@ -4,9 +4,10 @@ import com.vultisig.wallet.data.mappers.SuspendMapperFunc
 import com.vultisig.wallet.data.models.SwapProvider
 import com.vultisig.wallet.data.models.SwapTransaction
 import com.vultisig.wallet.data.repositories.AppCurrencyRepository
-import com.vultisig.wallet.data.repositories.SwapQuoteRepository
 import com.vultisig.wallet.data.repositories.TokenRepository
 import com.vultisig.wallet.data.usecases.ConvertTokenValueToFiatUseCase
+import com.vultisig.wallet.data.usecases.resolveprovider.ResolveProviderUseCase
+import com.vultisig.wallet.data.usecases.resolveprovider.SwapSelectionContext
 import com.vultisig.wallet.ui.models.swap.SwapTransactionUiModel
 import com.vultisig.wallet.ui.models.swap.ValuedToken
 import kotlinx.coroutines.flow.first
@@ -20,13 +21,19 @@ internal class SwapTransactionToUiModelMapperImpl @Inject constructor(
     private val fiatValueToStringMapper: FiatValueToStringMapper,
     private val convertTokenValueToFiat: ConvertTokenValueToFiatUseCase,
     private val appCurrencyRepository: AppCurrencyRepository,
-    private val swapQuoteRepository: SwapQuoteRepository,
+    private val resolveProviderUseCase: ResolveProviderUseCase,
     private val tokenRepository: TokenRepository,
 ) : SwapTransactionToUiModelMapper {
     override suspend fun invoke(from: SwapTransaction): SwapTransactionUiModel {
         val currency = appCurrencyRepository.currency.first()
-        val provider: SwapProvider = swapQuoteRepository
-            .resolveProvider(from.srcToken, from.dstToken) ?: error("provider not found")
+        val provider = resolveProviderUseCase(
+            SwapSelectionContext(
+                from.srcToken,
+                from.dstToken,
+                from.srcTokenValue
+            )
+        ) ?: error("provider not found")
+
         val tokenValue = when (provider) {
             SwapProvider.THORCHAIN, SwapProvider.MAYA ->
                 from.dstToken
