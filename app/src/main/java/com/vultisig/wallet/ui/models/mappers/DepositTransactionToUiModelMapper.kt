f185f8009582e289d9d65df35b0af7a34d45383b
JaimeToca <jaime.toca.munoz@gmail.com>
2025-07-27 19:07:53 +0700

    map value token values

--- app/src/main/java/com/vultisig/wallet/ui/models/mappers/DepositTransactionToUiModelMapper.kt
+++ app/src/main/java/com/vultisig/wallet/ui/models/mappers/DepositTransactionToUiModelMapper.kt
@@ -3,17 +3,23 @@ package com.vultisig.wallet.ui.models.mappers
 import com.vultisig.wallet.data.mappers.MapperFunc
 import com.vultisig.wallet.data.models.DepositTransaction
 import com.vultisig.wallet.ui.models.deposit.DepositTransactionUiModel
+import com.vultisig.wallet.ui.models.swap.ValuedToken
 import javax.inject.Inject
 
 internal interface DepositTransactionToUiModelMapper :
     MapperFunc<DepositTransaction, DepositTransactionUiModel>
 
-
 internal class DepositTransactionUiModelMapperImpl @Inject constructor(
     private val mapTokenValueToStringWithUnit: TokenValueToStringWithUnitMapper,
+    private val mapTokenValueToDecimalUiString: TokenValueToDecimalUiStringMapper,
 ) : DepositTransactionToUiModelMapper {
     override fun invoke(from: DepositTransaction): DepositTransactionUiModel =
         DepositTransactionUiModel(
+            token = ValuedToken(
+                token = from.srcToken,
+                value = mapTokenValueToDecimalUiString(from.srcTokenValue),
+                fiatValue = "",
+            ),
             fromAddress = from.srcAddress,
             srcTokenValue = mapTokenValueToStringWithUnit(from.srcTokenValue),
             estimatedFees = mapTokenValueToStringWithUnit(from.estimatedFees),
