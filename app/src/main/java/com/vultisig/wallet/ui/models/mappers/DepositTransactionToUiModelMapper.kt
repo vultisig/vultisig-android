14ca234e1a6adb68e2041b4f3edbfe4e9d5b1b69
JaimeToca <jaime.toca.munoz@gmail.com>
2025-07-27 16:29:58 +0700

    remove harcoded 0

--- app/src/main/java/com/vultisig/wallet/ui/models/mappers/DepositTransactionToUiModelMapper.kt
+++ app/src/main/java/com/vultisig/wallet/ui/models/mappers/DepositTransactionToUiModelMapper.kt
@@ -11,14 +11,13 @@ internal interface DepositTransactionToUiModelMapper :
 
 internal class DepositTransactionUiModelMapperImpl @Inject constructor(
     private val mapTokenValueToStringWithUnit: TokenValueToStringWithUnitMapper,
-    private val fiatValueToStringMapper: FiatValueToStringMapper,
 ) : DepositTransactionToUiModelMapper {
     override fun invoke(from: DepositTransaction): DepositTransactionUiModel =
         DepositTransactionUiModel(
             fromAddress = from.srcAddress,
             srcTokenValue = mapTokenValueToStringWithUnit(from.srcTokenValue),
             estimatedFees = mapTokenValueToStringWithUnit(from.estimatedFees),
-            estimateFeesFiat = "0",
+            estimateFeesFiat = from.estimateFeesFiat,
             memo = from.memo,
             nodeAddress = from.dstAddress,
         )
