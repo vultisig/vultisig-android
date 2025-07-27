1d6026032e355b8b418f209374b32974c2d0350e
Jaime Toca <jaime.toca.munoz@gmail.com>
2025-07-28 03:44:58 +0700

    [Feat]: Design improvements Overview Tx Screen (#2242)
    * avoid empty name
    
    * Create Generic Model
    
    * Deposit form
    
    * mapping transaction
    
    * change in Transaction Type
    
    * add extra info
    
    * remove useless code
    
    * refactor code
    
    * Create specific component and refactor
    
    * fix preview
    
    * support FiatValue in Deposit Txs
    
    * remove harcoded 0
    
    * remove harcoded strings
    
    * remove harcoded strings
    
    * map value token
    
    * map value token values
    
    * remove harcoded strings
    
    * add sign method
    
    * fix deposit fees

--- data/src/main/kotlin/com/vultisig/wallet/data/models/DepositTransaction.kt
+++ data/src/main/kotlin/com/vultisig/wallet/data/models/DepositTransaction.kt
@@ -5,12 +5,12 @@ import com.vultisig.wallet.data.models.payload.BlockChainSpecific
 data class DepositTransaction(
     val id: TransactionId,
     val vaultId: String,
-
     val srcToken: Coin,
     val srcAddress: String,
     val srcTokenValue: TokenValue,
     val memo: String,
     val dstAddress: String,
     val estimatedFees: TokenValue,
+    val estimateFeesFiat: String,
     val blockChainSpecific: BlockChainSpecific,
 )
