3f05988d8d19b1ce2aa0a451c97d382d69b917d4
aminsato <amin.saradar@yahoo.com>
2025-07-27 21:59:31 +0330

    Refactor

--- app/src/main/java/com/vultisig/wallet/ui/models/mappers/FiatValueToStringMapper.kt
+++ app/src/main/java/com/vultisig/wallet/ui/models/mappers/FiatValueToStringMapper.kt
@@ -2,35 +2,20 @@ package com.vultisig.wallet.ui.models.mappers
 
 import com.vultisig.wallet.data.mappers.SuspendMapperFunc
 import com.vultisig.wallet.data.models.FiatValue
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
-import java.text.NumberFormat
+import com.vultisig.wallet.data.repositories.AppCurrencyRepository
 import java.util.Currency
-import java.util.Locale
 import javax.inject.Inject
 
 internal interface FiatValueToStringMapper : SuspendMapperFunc<FiatValue, String>
 
-internal class FiatValueToStringMapperImpl @Inject constructor() : FiatValueToStringMapper {
-
-    private val mutex = Mutex()
-    private var cachedLocale: Locale? = null
-    private var cachedCurrencyFormat: NumberFormat? = null
+internal class FiatValueToStringMapperImpl @Inject constructor(
+    private val appCurrencyRepository: AppCurrencyRepository
+) : FiatValueToStringMapper {
 
     override suspend fun invoke(from: FiatValue): String = from.let {
-        val currencyFormat = getCurrencyFormat()
+        val currencyFormat = appCurrencyRepository.getCurrencyFormat()
         currencyFormat.currency = Currency.getInstance(it.currency)
         currencyFormat.format(it.value)
-    }
-
-    private suspend fun getCurrencyFormat(): NumberFormat = mutex.withLock {
-        val currentLocale = Locale.getDefault()
-        // Update the currency format in a thread-safe manner when the locale changes or the value is null
-        if (cachedLocale != currentLocale || cachedCurrencyFormat == null) {
-            cachedLocale = currentLocale
-            cachedCurrencyFormat = NumberFormat.getCurrencyInstance(currentLocale)
-        }
-        requireNotNull(cachedCurrencyFormat)
     }
 
 }
