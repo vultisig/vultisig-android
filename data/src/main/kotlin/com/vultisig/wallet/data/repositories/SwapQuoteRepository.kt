def8c4c5672cc17fc9da47a0dcd84e6324217596
aminsato <amin.saradar@yahoo.com>
2025-08-05 12:14:05 +0330

    Add TRON and NOBLE to Thorchain swaps

--- data/src/main/kotlin/com/vultisig/wallet/data/repositories/SwapQuoteRepository.kt
+++ data/src/main/kotlin/com/vultisig/wallet/data/repositories/SwapQuoteRepository.kt
@@ -580,7 +580,7 @@ internal class SwapQuoteRepositoryImpl @Inject constructor(
             )
 
             Chain.Dogecoin, Chain.BitcoinCash, Chain.Litecoin,
-            Chain.GaiaChain,
+            Chain.GaiaChain, Chain.Tron, Chain.Noble
                 -> setOf(
                 SwapProvider.THORCHAIN
             )
@@ -600,7 +600,7 @@ internal class SwapQuoteRepositoryImpl @Inject constructor(
 
             Chain.Ripple -> setOf(SwapProvider.THORCHAIN)
             Chain.Polkadot, Chain.Dydx, Chain.Sui, Chain.Ton, Chain.Osmosis,
-            Chain.Terra, Chain.TerraClassic, Chain.Noble, Chain.Akash, Chain.Tron, Chain.Cardano
+            Chain.Terra, Chain.TerraClassic, Chain.Akash, Chain.Cardano
                 -> emptySet()
         }
 
