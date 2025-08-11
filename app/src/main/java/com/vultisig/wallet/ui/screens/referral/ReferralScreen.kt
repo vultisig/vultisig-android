e7fc6c18d4d54ae895a6f8fcd7a77d395d400132
JaimeToca <jaime.toca.munoz@gmail.com>
2025-08-11 17:17:56 +0700

    start working on create screen

--- app/src/main/java/com/vultisig/wallet/ui/screens/referral/ReferralScreen.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/referral/ReferralScreen.kt
@@ -1,2 +1,36 @@
 package com.vultisig.wallet.ui.screens.referral
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
+import com.vultisig.wallet.R
+import com.vultisig.wallet.ui.components.topbar.VsTopAppBar
+import com.vultisig.wallet.ui.theme.Theme
+
+@Composable
+fun ReferralScreen(
+    navController: NavController,
+) {
+
+    Scaffold(
+        containerColor = Theme.colors.backgrounds.primary,
+        topBar = {
+            VsTopAppBar(
+                title = stringResource(R.string.migration_onboarding_upgrade_your_vault),
+                onBackClick = {
+
+                },
+            )
+        },
+        content = { contentPadding ->
+            ReferralContent(contentPadding)
+        },
+    )
+}
+
+@Composable
+internal fun ReferralContent(paddingValues: PaddingValues) {
+
+}
