562308874d56d6ca3ab8ee5bedc22e2d7e508b69
JaimeToca <jaime.toca.munoz@gmail.com>
2025-08-12 15:38:23 +0700

    load initial status

--- app/src/main/java/com/vultisig/wallet/ui/models/referral/ReferralViewModel.kt
+++ app/src/main/java/com/vultisig/wallet/ui/models/referral/ReferralViewModel.kt
@@ -3,17 +3,21 @@ package com.vultisig.wallet.ui.models.referral
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.vultisig.wallet.data.repositories.ReferralCodeSettingsRepository
 import com.vultisig.wallet.ui.navigation.Destination
 import com.vultisig.wallet.ui.navigation.Navigator
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 internal data class ReferralUiState(
     val referralCode: String = "",
     val errorMessage: String? = null,
     val isLoading: Boolean = false,
-    val isSaveEnabled: Boolean = false,
+    val isSaveEnabled: Boolean = true,
     val isCreateEnabled: Boolean = true,
 )
 
@@ -21,13 +25,31 @@ internal data class ReferralUiState(
 internal class ReferralViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
     private val navigator: Navigator<Destination>,
-    private val referralCodeSettingsRepository: ReferralCodeSettingsRepository,
+    private val referralCodeRepository: ReferralCodeSettingsRepository,
 ) : ViewModel() {
+    private val vaultId: String = ""
 
     val referralCodeTextFieldState = TextFieldState()
+    val state = MutableStateFlow(ReferralUiState())
 
     init {
+        loadStatus()
+    }
 
+    private fun loadStatus() {
+        viewModelScope.launch {
+            val vaultReferral = referralCodeRepository.getReferralCreatedBy(vaultId)
+            val externalReferral = referralCodeRepository.getExternalReferralBy(vaultId)
+
+            state.update {
+                it.copy(
+                    referralCode = externalReferral ?: "",
+                    isLoading = false,
+                    isSaveEnabled = externalReferral.isNullOrEmpty(),
+                    isCreateEnabled = vaultReferral.isNullOrEmpty(),
+                )
+            }
+        }
     }
 
     fun onCreateOrEditReferral() {
