791e1f1c8d75d25df2d2eebd636778cab161fb28
JaimeToca <jaime.toca.munoz@gmail.com>
2025-08-13 17:37:00 +0700

    Rerral onboarding screen

--- app/src/main/java/com/vultisig/wallet/ui/screens/referral/ReferralOnboardingScreen.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/referral/ReferralOnboardingScreen.kt
@@ -1,2 +1,52 @@
 package com.vultisig.wallet.ui.screens.referral
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.vultisig.wallet.R
+import com.vultisig.wallet.ui.components.UiSpacer
+import com.vultisig.wallet.ui.components.buttons.VsButton
+import com.vultisig.wallet.ui.components.rive.RiveAnimation
+import com.vultisig.wallet.ui.components.util.BlockBackClick
+import com.vultisig.wallet.ui.theme.Theme
+
+@Composable
+internal fun ReferralOnboardingScreen() {
+    BlockBackClick()
+
+    Column(
+        modifier = Modifier
+            .background(Theme.colors.backgrounds.primary)
+            .fillMaxSize(),
+    ) {
+        Box(
+            modifier = Modifier
+                .background(Theme.colors.backgrounds.primary)
+                .weight(1f)
+                .fillMaxSize(),
+        ) {
+            RiveAnimation(
+                modifier = Modifier
+                    .align(Alignment.Center),
+                animation = R.raw.riv_securevault_summary, // Waiting for designer to provider riv
+            )
+        }
+
+        VsButton(
+            onClick = { },
+            label = "Get Started",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        )
+
+        UiSpacer(32.dp)
+    }
+}
