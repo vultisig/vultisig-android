df4544c0b7dbb8e5116b71603f6ef468959edf4c
aminsato <amin.saradar@yahoo.com>
2025-08-19 14:32:51 +0330

    Update build.gradle.kts

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -30,18 +30,6 @@ android {
             )
         }
     }
-
-    packaging {
-        resources {
-           pickFirsts.add("META-INF/DEPENDENCIES")
-            pickFirsts.add("META-INF/LICENSE")
-            pickFirsts.add("META-INF/LICENSE.txt")
-            pickFirsts.add("META-INF/NOTICE")
-            pickFirsts.add("META-INF/NOTICE.txt")
-            pickFirsts.add("META-INF/ASL2.0")
-        }
-    }
-
     sourceSets.getByName("main") {
         proto {
             srcDir("${project.rootProject.rootDir}/commondata/proto")
