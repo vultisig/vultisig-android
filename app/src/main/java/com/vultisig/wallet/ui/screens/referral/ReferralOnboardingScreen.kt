7cc2fb701574a38ad2f1408153d047af8229b441
JaimeToca <jaime.toca.munoz@gmail.com>
2025-08-19 17:06:20 +0700

    Add support for timeline line

--- app/src/main/java/com/vultisig/wallet/ui/screens/referral/ReferralOnboardingScreen.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/referral/ReferralOnboardingScreen.kt
@@ -3,18 +3,22 @@ package com.vultisig.wallet.ui.screens.referral
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -23,7 +27,6 @@ import com.vultisig.wallet.ui.components.UiSpacer
 import com.vultisig.wallet.ui.components.buttons.VsButton
 import com.vultisig.wallet.ui.components.referral.VsPromoBox
 import com.vultisig.wallet.ui.components.referral.VsPromoTag
-import com.vultisig.wallet.ui.components.rive.RiveAnimation
 import com.vultisig.wallet.ui.components.topbar.VsTopAppBar
 import com.vultisig.wallet.ui.models.referral.OnBoardingReferralViewModel
 import com.vultisig.wallet.ui.theme.Theme
@@ -53,66 +56,180 @@ internal fun ReferralOnboardingScreen(
                 .verticalScroll(rememberScrollState())
                 .padding(16.dp),
         ) {
-            VsPromoTag(
-                icon = R.drawable.ic_trumpet,
-                text = stringResource(R.string.referral_program_tag)
-            )
+            ReferralTag()
 
-            Text(
-                text = stringResource(R.string.referral_how_it_works),
-                style = Theme.brockmann.headings.largeTitle,
-                color = Theme.colors.text.primary,
-                modifier = Modifier.padding(top = 32.dp, bottom = 32.dp, start = 48.dp)
-            )
+            HowItWorksTitle()
 
-            VsPromoBox(
-                title = stringResource(R.string.referral_create_code_title),
-                description = stringResource(R.string.referral_create_code_description),
-                icon = R.drawable.ic_referral,
-                modifier = Modifier.padding(start = 48.dp, end = 16.dp),
-            )
-
-            UiSpacer(16.dp)
-
-            VsPromoBox(
-                title = stringResource(R.string.referral_share_title),
-                description = stringResource(R.string.referral_share_description),
-                icon = R.drawable.ic_share_referral,
-                modifier = Modifier.padding(start = 48.dp, end = 16.dp),
-            )
-
-            UiSpacer(16.dp)
-
-            VsPromoBox(
-                title = stringResource(R.string.referral_earn_title),
-                description = stringResource(R.string.referral_earn_description),
-                icon = R.drawable.ic_cup,
-                modifier = Modifier.padding(start = 48.dp, end = 16.dp),
-            )
-
-            UiSpacer(16.dp)
-
-            VsPromoBox(
-                title = stringResource(R.string.referral_use_code_title),
-                description = stringResource(R.string.referral_use_code_description),
-                icon = R.drawable.ic_user,
-                modifier = Modifier.padding(start = 48.dp, end = 16.dp),
-            )
+            TimeLineList()
 
             UiSpacer(1f)
 
-            VsButton(
-                onClick = {
-                    model.onClickGetStarted()
-                },
-                label = stringResource(R.string.referral_onboarding_get_started),
+            FooterButton(model)
+        }
+    }
+}
+
+@Composable
+private fun FooterButton(model: OnBoardingReferralViewModel) {
+    VsButton(
+        onClick = {
+            model.onClickGetStarted()
+        },
+        label = stringResource(R.string.referral_onboarding_get_started),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .navigationBarsPadding()
+    )
+
+    UiSpacer(32.dp)
+}
+
+@Composable
+private fun TimeLineList() {
+    TimelineItem(
+        hasLineBelow = true,
+        spacerHeight = 16.dp
+    ) {
+        VsPromoBox(
+            title = stringResource(R.string.referral_create_code_title),
+            description = stringResource(R.string.referral_create_code_description),
+            icon = R.drawable.ic_referral,
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
+
+    TimelineItem(
+        hasLineBelow = true,
+        spacerHeight = 16.dp
+    ) {
+        VsPromoBox(
+            title = stringResource(R.string.referral_share_title),
+            description = stringResource(R.string.referral_share_description),
+            icon = R.drawable.ic_share_referral,
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
+
+    TimelineItem(
+        hasLineBelow = true,
+        spacerHeight = 16.dp
+    ) {
+        VsPromoBox(
+            title = stringResource(R.string.referral_earn_title),
+            description = stringResource(R.string.referral_earn_description),
+            icon = R.drawable.ic_cup,
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
+
+    TimelineItem(
+        hasLineBelow = false,
+        spacerHeight = 0.dp
+    ) {
+        VsPromoBox(
+            title = stringResource(R.string.referral_use_code_title),
+            description = stringResource(R.string.referral_use_code_description),
+            icon = R.drawable.ic_user,
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
+}
+
+@Composable
+private fun HowItWorksTitle() {
+    Box {
+        Box(
+            modifier = Modifier
+                .padding(start = 24.dp)
+                .width(1.dp)
+                .height(110.dp)
+                .background(Theme.colors.borders.light)
+        )
+
+        Text(
+            text = stringResource(R.string.referral_how_it_works),
+            style = Theme.brockmann.headings.largeTitle,
+            color = Theme.colors.text.primary,
+            modifier = Modifier.padding(top = 32.dp, bottom = 32.dp, start = 48.dp)
+        )
+    }
+}
+
+@Composable
+private fun ReferralTag() {
+    Box {
+        Box(
+            modifier = Modifier
+                .padding(start = 24.dp, top = 20.dp)
+                .width(1.dp)
+                .wrapContentHeight()
+                .background(Theme.colors.borders.light)
+        )
+        VsPromoTag(
+            icon = R.drawable.ic_trumpet,
+            text = stringResource(R.string.referral_program_tag)
+        )
+    }
+}
+
+@Composable
+private fun TimelineItem(
+    hasLineBelow: Boolean = true,
+    spacerHeight: Dp = 16.dp,
+    content: @Composable () -> Unit
+) {
+    Box {
+        // Vertical line that goes through item
+        Column {
+            val verticalHeight = if (hasLineBelow) {
+                72.dp
+            } else {
+                24.dp
+            }
+            Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-                    .navigationBarsPadding()
+                    .padding(start = 24.dp)
+                    .width(1.dp)
+                    .height(verticalHeight)
+                    .background(Theme.colors.borders.light)
             )
 
-            UiSpacer(32.dp)
+            // If last one, only draw half
+            if (hasLineBelow && spacerHeight > 0.dp) {
+                Box(
+                    modifier = Modifier
+                        .padding(start = 24.dp)
+                        .width(1.dp)
+                        .height(spacerHeight)
+                        .background(Theme.colors.borders.light)
+                )
+            }
+        }
+
+        // Horizontal line + actual item
+        Row(
+            verticalAlignment = Alignment.Top,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Box(
+                modifier = Modifier.padding(top = 24.dp),
+            ) {
+                Box(
+                    modifier = Modifier
+                        .padding(start = 25.dp)
+                        .width(23.dp)
+                        .height(1.dp)
+                        .background(Theme.colors.borders.light)
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                content()
+            }
         }
     }
 }
