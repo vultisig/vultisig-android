556a8d8b5ab26896d5fa2cf542fd31bd1432cbed
JaimeToca <jaime.toca.munoz@gmail.com>
2025-08-21 16:33:10 +0700

    fix terms

--- app/src/main/java/com/vultisig/wallet/ui/utils/VsAuxiliaryLinks.kt
+++ app/src/main/java/com/vultisig/wallet/ui/utils/VsAuxiliaryLinks.kt
@@ -4,7 +4,7 @@ object VsAuxiliaryLinks {
     const val CREATE_VAULT = "https://docs.vultisig.com/vultisig-user-actions/creating-a-vault"
     const val AIRDROP = "https://airdrop.vultisig.com"
     const val PRIVACY = "https://vultisig.com/privacy"
-    const val TERMS_OF_SERVICE = "https://vultisig.com/termofservice"
+    const val TERMS_OF_SERVICE = "https://vultisig.com/terms"
     const val GITHUB = "https://github.com/vultisig"
     const val TWITTER = "https://twitter.com/vultisig"
     const val DISCORD = "https://discord.gg/54wEtGYxuv"
