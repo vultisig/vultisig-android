350d7d5a8036f304d9ea47c676dc17d1729dc456
hailhydra <johnnyluo1980@gmail.com>
2025-09-08 16:12:52 +1000

    Bump version to 1.0.72 (#2441)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.vultisig.wallet"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = 35
-        versionCode = 71
-        versionName = "1.0.71"
+        versionCode = 72
+        versionName = "1.0.72"
 
         testInstrumentationRunner = "com.vultisig.wallet.util.HiltTestRunner"
 
