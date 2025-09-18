548d8edc869752d11f066fab42d490f3b9bda02b
aminsato <amin.saradar@yahoo.com>
2025-09-18 19:58:16 +0330

    Add ChainSelectionItem component for selecting blockchain networks

---
+++ app/src/main/java/com/vultisig/wallet/ui/screens/v2/home/bottomsheets/ChainSelectionItem.kt
@@ -0,0 +1,255 @@
+package com.vultisig.wallet.ui.screens.v2.home.bottomsheets
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.RoundRect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathOperation
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.vultisig.wallet.data.models.Chain
+import com.vultisig.wallet.data.models.logo
+import com.vultisig.wallet.ui.components.UiSpacer
+import com.vultisig.wallet.ui.theme.Theme
+
+@Composable
+internal fun ChainSelectionItem(
+    modifier: Modifier = Modifier,
+    chain: Chain,
+    isChecked: Boolean,
+    onCheckedChange: (Boolean) -> Unit = {}
+) {
+
+    Column(
+        modifier = modifier.clickable(
+            onClick = {
+                onCheckedChange(!isChecked)
+            }
+        ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Box(
+            contentAlignment = Alignment.Center
+        ) {
+            RoundedBorderWithLeaf(
+                isSelected = isChecked,
+            )
+
+            Image(
+                painter = painterResource(chain.logo),
+                contentDescription = null,
+                modifier = Modifier.size(28.dp)
+            )
+        }
+
+        UiSpacer(
+            size = 10.dp
+        )
+
+        Text(
+            text = chain.raw,
+            style = Theme.brockmann.supplementary.caption,
+            color = Theme.colors.text.primary
+        )
+    }
+
+
+}
+
+@Composable
+internal fun Dp.toPx() = with(LocalDensity.current) {
+    toPx()
+}
+
+@Composable
+internal fun RoundedBorderWithLeaf(
+    modifier: Modifier = Modifier,
+    borderSize: Dp = 74.dp,
+    isSelected: Boolean,
+    borderColor: Color = Theme.colors.borders.normal,
+    backgroundColor: Color = Theme.colors.backgrounds.primary,
+    leafColor: Color = Theme.colors.borders.normal,
+    checkMarkColor: Color = Theme.colors.alerts.success,
+    borderWidth: Dp = 1.5.dp,
+    cornerRadius: Dp = 24.dp,
+    leafXLength: Dp = 32.dp,
+    leafYLength: Dp = 24.dp,
+    checkMarkScale: Float = 2f,
+    checkMarkWidth: Float = 4f,
+    checkMarkOffset: Offset = Offset(-4f, 8f)
+) {
+
+    val cornerRadiusPx = cornerRadius.toPx()
+    val borderWidthPx = borderWidth.toPx()
+    val leafWidthPx = leafXLength.toPx()
+    val leafHeighPx = leafYLength.toPx()
+
+
+    Canvas(
+        modifier = modifier.size(borderSize)
+    ) {
+        val canvasWidth = size.width
+        val borderExcludedWidth = canvasWidth - borderWidthPx
+        val canvasHeight = size.height
+        val borderExcludedHeight = canvasHeight - borderWidthPx
+
+
+        val backgroundPath = Path().apply {
+            addRoundRect(
+                RoundRect(
+                    left = 0f,
+                    top = 0f,
+                    right = canvasWidth,
+                    bottom = canvasHeight,
+                    cornerRadius = CornerRadius(
+                        x = cornerRadiusPx,
+                        y = cornerRadiusPx,
+                    )
+                )
+            )
+        }
+
+        drawPath(
+            path = backgroundPath,
+            color = backgroundColor,
+        )
+        val borderStartTop = borderWidthPx / 2
+        val borderStartLeft = borderWidthPx / 2
+
+        val borderPath = Path().apply {
+            addRoundRect(
+                RoundRect(
+                    left = borderStartLeft,
+                    top = borderStartTop,
+                    right = borderStartLeft + borderExcludedWidth,
+                    bottom = borderStartTop + borderExcludedHeight,
+                    cornerRadius = CornerRadius(
+                        x = cornerRadiusPx,
+                        y = cornerRadiusPx,
+                    )
+                )
+            )
+        }
+
+        if (isSelected) {
+            drawPath(
+                path = borderPath,
+                color = borderColor,
+                style = Stroke(
+                    width = borderWidthPx,
+                    cap = StrokeCap.Round,
+                    join = StrokeJoin.Round
+                )
+            )
+        }
+
+        if (isSelected.not())
+            return@Canvas
+
+        val leafStartTop = borderExcludedHeight - leafHeighPx
+        val leafStartLeft = borderExcludedWidth - leafWidthPx
+        val leafPath = Path()
+
+        leafPath.op(
+            path1 = borderPath,
+            path2 = Path().apply {
+                addRoundRect(
+                    RoundRect(
+                        left = leafStartLeft,
+                        top = leafStartTop,
+                        right = leafStartLeft + borderExcludedWidth,
+                        bottom = leafStartTop + borderExcludedHeight,
+                        cornerRadius = CornerRadius(
+                            x = cornerRadiusPx,
+                            y = cornerRadiusPx
+                        )
+                    )
+                )
+            },
+            operation = PathOperation.Intersect
+        )
+
+
+        drawPath(
+            path = leafPath,
+            color = leafColor,
+        )
+
+        val checkMarkStartPoint = Offset(
+            x = leafPath.getBounds().center.x + checkMarkOffset.x,
+            y = leafPath.getBounds().center.y + checkMarkOffset.y
+        )
+
+        val checkMarkPath = Path().apply {
+            moveTo(
+                Offset(
+                    x = checkMarkStartPoint.x - 2f * checkMarkScale,
+                    y = checkMarkStartPoint.y - 2f * checkMarkScale
+                ).x, Offset(
+                    x = checkMarkStartPoint.x - 2f * checkMarkScale,
+                    y = checkMarkStartPoint.y - 2f * checkMarkScale
+                ).y
+            )
+            lineTo(
+                x = checkMarkStartPoint.x,
+                y = checkMarkStartPoint.y
+            )
+            lineTo(
+                x = Offset(
+                    x = checkMarkStartPoint.x + 5f * checkMarkScale,
+                    y = checkMarkStartPoint.y - 6f * checkMarkScale
+                ).x,
+                y = Offset(
+                    x = checkMarkStartPoint.x + 5f * checkMarkScale,
+                    y = checkMarkStartPoint.y - 6f * checkMarkScale
+                ).y
+            )
+        }
+
+        drawPath(
+            checkMarkPath,
+            checkMarkColor,
+            style = Stroke(
+                width = checkMarkWidth,
+                cap = StrokeCap.Round,
+                join = StrokeJoin.Round
+            )
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewRChainSelectionItem() {
+    ChainSelectionItem(
+        chain = Chain.Bitcoin,
+        isChecked = true
+    )
+}
+
+@Preview
+@Composable
+fun PreviewChainSelectionItem2() {
+    ChainSelectionItem(
+        chain = Chain.Bitcoin,
+        isChecked = false
+    )
+}
+
