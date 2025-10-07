2e8d47ffd9f8fd8df5069fe47a9108581e890ce5
Jaime Toca <jaime.toca.munoz@gmail.com>
2025-10-07 17:58:04 +0700

    [Biometrics]: Toggeling off fast sign needs password (#2577)
    * change biometrict
    
    * fix snackbar
    
    * fix snackbar
    
    * fix snackbar
    
    ---------
    
    Co-authored-by: hailhydra <johnnyluo1980@gmail.com>

--- app/src/main/java/com/vultisig/wallet/ui/screens/vault_settings/VaultSettingsViewModel.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/vault_settings/VaultSettingsViewModel.kt
@@ -354,11 +354,17 @@ internal open class VaultSettingsViewModel @Inject constructor(
             }
 
             is VaultSettingsItem.BiometricFastSign -> {
-                val newItem = uiModel.value.copy(
-                    isBiometricFastSignBottomSheetVisible = true,
-                )
-                uiModel.update {
-                    newItem
+                viewModelScope.launch {
+                    if (!item.isBiometricEnabled) {
+                        val newItem = uiModel.value.copy(
+                            isBiometricFastSignBottomSheetVisible = true,
+                        )
+                        uiModel.update { newItem }
+                    } else {
+                        vaultPasswordRepository.clearPassword(vaultId)
+                        isBiometricFastSignEnabled = false
+                        showSnackbarMessage()
+                    }
                 }
             }
 
@@ -531,13 +537,8 @@ internal open class VaultSettingsViewModel @Inject constructor(
             isBiometricFastSignEnabled = true
             passwordTextFieldState.clearText()
             hideBiometricFastVaultBottomSheet()
-        } else {
-            vaultPasswordRepository.clearPassword(vaultId)
-            isBiometricFastSignEnabled = false
-            passwordTextFieldState.clearText()
-            hideBiometricFastVaultBottomSheet()
+            showSnackbarMessage()
         }
-        showSnackbarMessage()
     }
 
     private fun hideBiometricFastVaultBottomSheet() {
