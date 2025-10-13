816d19da1ea4b7dc364c0c28b9a3ebb032c655d2
Amin <32006742+aminsato@users.noreply.github.com>
2025-10-13 13:06:16 +0330

    Fix palce holders height (#2600)
    * Fix banners height
    
    * Resolve code rabbit comments
    
    * update
    
    ---------
    
    Co-authored-by: hailhydra <johnnyluo1980@gmail.com>

--- app/src/main/java/com/vultisig/wallet/ui/screens/v2/home/pager/banner/UpgradeBanner.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/v2/home/pager/banner/UpgradeBanner.kt
@@ -5,16 +5,17 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,7 +32,7 @@ internal fun UpgradeBanner(
     onUpgradeClick: () -> Unit = {},
 ) {
     Row(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Column(
@@ -81,14 +82,16 @@ internal fun UpgradeBanner(
 
         Box(
             modifier = Modifier
-                .fillMaxSize()
+                .wrapContentSize(align = Alignment.BottomEnd)
         ) {
             Image(
                 painter = painterResource(
                     id = R.drawable.upgrade_vault_v2
                 ),
                 contentDescription = null,
+                contentScale = ContentScale.FillHeight,
                 modifier = Modifier
+                    .size(140.dp)
                     .align(alignment = Alignment.BottomEnd)
             )
         }
@@ -102,7 +105,6 @@ private fun PreviewUpgradeBanner() {
         modifier =
             Modifier
                 .fillMaxWidth()
-                .height(140.dp)
     ) {
         UpgradeBanner()
     }
