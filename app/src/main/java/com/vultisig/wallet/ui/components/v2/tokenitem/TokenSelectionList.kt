8c275b3a93f84936ffd81a3a59dccb6fe1e49532
Amin <32006742+aminsato@users.noreply.github.com>
2025-10-18 14:15:57 +0330

    Redesign token list screen (#2638)
    * Refactor chain selection UI components
    
    * fix crash
    
    * Update VerifyDepositViewModel.kt
    
    * Enhance Chain Selection Screen UI and improve error handling in ViewModels
    
    ---------
    
    Co-authored-by: JaimeToca <jaime.toca.munoz@gmail.com>
    Co-authored-by: hailhydra <johnnyluo1980@gmail.com>

---
+++ app/src/main/java/com/vultisig/wallet/ui/components/v2/tokenitem/TokenSelectionList.kt
@@ -0,0 +1,103 @@
+package com.vultisig.wallet.ui.components.v2.tokenitem
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.vultisig.wallet.R
+import com.vultisig.wallet.ui.components.UiSpacer
+import com.vultisig.wallet.ui.components.v2.bottomsheets.V2BottomSheet
+import com.vultisig.wallet.ui.components.v2.buttons.VsCircleButton
+import com.vultisig.wallet.ui.components.v2.buttons.VsCircleButtonSize
+import com.vultisig.wallet.ui.components.v2.buttons.VsCircleButtonType
+import com.vultisig.wallet.ui.components.v2.searchbar.SearchBar
+
+@Composable
+internal fun <T> TokenSelectionList(
+    items: List<T>,
+    mapper: (T) -> TokenSelectionGridUiModel,
+    searchTextFieldState: TextFieldState,
+    titleContent: @Composable () -> Unit,
+    notFoundContent: @Composable () -> Unit,
+    onCheckChange: (Boolean, T) -> Unit,
+    onDoneClick: () -> Unit,
+    onCancelClick: () -> Unit,
+    onPlusClick: (() -> Unit)? = null,
+) {
+    V2BottomSheet(
+        onDismissRequest = onCancelClick,
+        leftAction = {
+            VsCircleButton(
+                drawableResId = R.drawable.big_close,
+                onClick = onCancelClick,
+                type = VsCircleButtonType.Tertiary,
+                size = VsCircleButtonSize.Small,
+            )
+        },
+        rightAction = {
+            VsCircleButton(
+                drawableResId = R.drawable.big_tick,
+                onClick = onDoneClick,
+                size = VsCircleButtonSize.Small,
+            )
+
+        }
+    ) {
+        Column(
+            modifier = Modifier.Companion
+                .fillMaxSize(),
+        ) {
+            UiSpacer(
+                size = 24.dp
+            )
+
+            titleContent()
+
+            UiSpacer(
+                size = 16.dp
+            )
+
+            SearchBar(
+                state = searchTextFieldState,
+                onCancelClick = {},
+                isInitiallyFocused = false
+            )
+            UiSpacer(
+                size = 24.dp
+            )
+
+            if (items.isEmpty()) {
+                notFoundContent()
+            } else
+                LazyVerticalGrid(
+                    columns = GridCells.Adaptive(minSize = 74.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                ) {
+                    onPlusClick?.let {
+                        item {
+                            GridPlus(
+                                title = "Custom",
+                                onClick = it
+                            )
+                        }
+                    }
+                    items(items) { item ->
+                        TokenSelectionGridItem(
+                            uiModel = mapper(item),
+                            onCheckedChange = {
+                                onCheckChange(it, item)
+                            }
+                        )
+                    }
+                }
+        }
+    }
+
+}
