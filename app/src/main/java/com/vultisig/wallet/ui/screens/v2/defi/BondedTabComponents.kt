7893d487057140bbaaebffed70e19989a4dfb112
JaimeToca <jaime.toca.munoz@gmail.com>
2025-10-25 21:45:31 +0700

    create file components

---
+++ app/src/main/java/com/vultisig/wallet/ui/screens/v2/defi/BondedTabComponents.kt
@@ -0,0 +1,133 @@
+package com.vultisig.wallet.ui.screens.v2.defi
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.vultisig.wallet.R
+import com.vultisig.wallet.ui.components.UiHorizontalDivider
+import com.vultisig.wallet.ui.components.UiSpacer
+import com.vultisig.wallet.ui.components.buttons.VsButton
+import com.vultisig.wallet.ui.components.buttons.VsButtonState
+import com.vultisig.wallet.ui.components.clickOnce
+import com.vultisig.wallet.ui.screens.v2.home.components.NotEnabledContainer
+import com.vultisig.wallet.ui.theme.Theme
+
+@Composable
+fun BondedTabContent(managePositionsOnClick: () -> Unit, bondToNodeOnClick: () -> Unit) {
+    TotalBondWidget(
+        onClickBondToNode = bondToNodeOnClick,
+    )
+
+    NotEnabledContainer(
+        title = stringResource(R.string.defi_no_positions_selected),
+        content = stringResource(R.string.defi_no_positions_selected_desc),
+        action = {
+            Text(
+                text = "Manage Positions",
+                style = Theme.brockmann.button.medium,
+                color = Theme.colors.text.primary,
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .clickOnce(onClick = managePositionsOnClick)
+                    .background(
+                        color = Theme.v2.colors.border.primaryAccent4
+                    )
+                    .padding(
+                        vertical = 8.dp,
+                        horizontal = 16.dp
+                    )
+            )
+        }
+    )
+}
+
+@Composable
+fun TotalBondWidget(
+    onClickBondToNode: () -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(16.dp))
+            .background(Theme.colors.backgrounds.secondary)
+            .border(
+                width = 1.dp,
+                color = Theme.colors.borders.normal,
+                shape = RoundedCornerShape(16.dp)
+            )
+            .padding(16.dp)
+    ) {
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = painterResource(id = R.drawable.rune),
+                contentDescription = null,
+                modifier = Modifier.size(36.dp)
+            )
+
+            UiSpacer(12.dp)
+
+            Column {
+                Text(
+                    text = "Total Bonded Rune",
+                    style = Theme.brockmann.supplementary.footnote,
+                    color = Theme.v2.colors.text.extraLight,
+                )
+
+                UiSpacer(4.dp)
+
+                Text(
+                    text = "0 RUNE",
+                    style = Theme.brockmann.headings.title1,
+                    color = Theme.colors.text.primary,
+                )
+            }
+        }
+
+        UiSpacer(16.dp)
+
+        UiHorizontalDivider(color = Theme.v2.colors.border.light)
+
+        UiSpacer(16.dp)
+
+        VsButton(
+            label = "Bond to Node",
+            modifier = Modifier.fillMaxWidth(),
+            onClick = onClickBondToNode,
+            state = VsButtonState.Enabled,
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+private fun BondedTabContentPreview() {
+    BondedTabContent(
+        managePositionsOnClick = {},
+        bondToNodeOnClick = {}
+    )
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+private fun TotalBondWidgetPreview() {
+    TotalBondWidget(
+        onClickBondToNode = {}
+    )
+}
