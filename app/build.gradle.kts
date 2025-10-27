955680f34aa8e3a2fee477273276f4cbfcc5a271
Johnny Luo <johnnyluo1980@gmail.com>
2025-10-27 19:48:02 +1100

    Bump version to 1.0.80

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.vultisig.wallet"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = 35
-        versionCode = 79
-        versionName = "1.0.79"
+        versionCode = 80
+        versionName = "1.0.80"
 
         testInstrumentationRunner = "com.vultisig.wallet.util.HiltTestRunner"
 
