d3f9bcccc675a87f97ba8b95b4bead9480268881
Amin <32006742+aminsato@users.noreply.github.com>
2025-11-01 02:45:19 +0330

    Export multiple vaults in a .zip  (#2682)
    * Refactor backup navigation and introduce BackupType for improved vault backup handling
    
    * Enhance backup functionality by introducing MIME type handling and supporting ZIP format for vault backups
    
    * Log error when failing to save ZIP content and update remainedCount to null in backup model
    
    * Update VaultsToBackupScreen.kt
    
    * Update VaultToBackupUiModel.kt
    
    * Improve error handling in BackupPasswordViewModel
    
    * Format ZIP backup filename to ensure consistent two-digit time components
    
    ---------
    
    Co-authored-by: hailhydra <johnnyluo1980@gmail.com>

--- app/src/main/java/com/vultisig/wallet/ui/screens/backup/VaultToBackupUiModel.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/backup/VaultToBackupUiModel.kt
@@ -1,12 +1,21 @@
 package com.vultisig.wallet.ui.screens.backup
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.toRoute
+import com.vultisig.wallet.data.models.Vault
+import com.vultisig.wallet.data.models.getVaultPart
+import com.vultisig.wallet.data.models.isFastVault
+import com.vultisig.wallet.data.repositories.VaultRepository
+import com.vultisig.wallet.ui.navigation.BackupType
 import com.vultisig.wallet.ui.navigation.Destination
 import com.vultisig.wallet.ui.navigation.Navigator
+import com.vultisig.wallet.ui.navigation.Route
 import com.vultisig.wallet.ui.navigation.back
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,18 +29,41 @@ internal data class VaultToBackupUiModel(
 internal data class BackupVaultUiModel(
     val currentVault: VaultToBackupUiModel = VaultToBackupUiModel(),
     val vaultsToBackup: List<VaultToBackupUiModel> = emptyList(),
-    val remainedCount: Int? = null,
 )
 
 
 @HiltViewModel
 internal class VaultsToBackupViewModel @Inject constructor(
-    private val navigator: Navigator<Destination>
+    private val vaultRepository: VaultRepository,
+    private val navigator: Navigator<Destination>,
+    savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
+
+    private val vaultId = savedStateHandle.toRoute<Route.VaultsToBackup>().vaultId
+
+    init {
+        loadData()
+    }
 
     val uiState = MutableStateFlow(
         BackupVaultUiModel()
     )
+
+    private fun loadData() {
+        viewModelScope.launch {
+            val currentVault = vaultRepository.get(vaultId)?.toUi()
+                ?: error("Vault not found")
+            val allVaults = vaultRepository.getAll().map {
+                it.toUi()
+            }
+            uiState.update {
+                it.copy(
+                    currentVault = currentVault,
+                    vaultsToBackup = allVaults,
+                )
+            }
+        }
+    }
 
 
     fun back() {
@@ -39,4 +71,32 @@ internal class VaultsToBackupViewModel @Inject constructor(
             navigator.back()
         }
     }
+
+    fun backupCurrentVault() {
+        viewModelScope.launch {
+            navigator.route(
+                Route.BackupPasswordRequest(
+                    vaultId = vaultId,
+                    backupType = BackupType.CurrentVault(),
+                )
+            )
+        }
+    }
+
+    fun backupAllVaults() {
+        viewModelScope.launch {
+            navigator.route(Route.BackupPasswordRequest(
+                vaultId = vaultId,
+                backupType = BackupType.AllVaults
+            ))
+        }
+    }
+
+    private fun Vault.toUi() = VaultToBackupUiModel(
+        name = name,
+        part = getVaultPart(),
+        size = signers.size,
+        isFast = isFastVault(),
+    )
+
 }
