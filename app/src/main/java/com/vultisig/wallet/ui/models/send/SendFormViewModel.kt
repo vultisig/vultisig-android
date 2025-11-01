b26af6bd4c8b8d3e3da0ae42a2d67c2d941bb0eb
aminsato <amin.saradar@yahoo.com>
2025-11-01 10:25:56 +0330

    Update Deprecated Code

--- app/src/main/java/com/vultisig/wallet/ui/models/send/SendFormViewModel.kt
+++ app/src/main/java/com/vultisig/wallet/ui/models/send/SendFormViewModel.kt
@@ -76,6 +76,7 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.launchIn
@@ -1026,7 +1027,9 @@ internal class SendFormViewModel @Inject constructor(
                     .combine(memoFieldState.textAsFlow()) { (token, dst), memo ->
                         Triple(token, dst, memo.toString())
                     }
-                    .debounce(350)
+                    .map { triple -> triple }
+                    .onEach { _ -> delay(350) }
+                    .distinctUntilChanged()
                     .map { (token, dst, memo) ->
                         gasFeeRepository.getGasFee(
                             chain = token.chain,
