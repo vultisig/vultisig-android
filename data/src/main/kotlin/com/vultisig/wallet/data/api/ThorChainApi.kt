9127b5b21a49f494f4558478130a17d3372bbbfb
JaimeToca <jaime.toca.munoz@gmail.com>
2025-11-04 13:45:04 +0700

    Fix serialization issues

--- data/src/main/kotlin/com/vultisig/wallet/data/api/ThorChainApi.kt
+++ data/src/main/kotlin/com/vultisig/wallet/data/api/ThorChainApi.kt
@@ -922,11 +922,12 @@ data class TcyStakersResponse(
     )
 }
 
-
+@Serializable
 data class ThorchainConstantsResponse(
     @SerialName("int_64_values")
     val int64Values: Int64Values
 ) {
+    @Serializable
     data class Int64Values(
         @SerialName("MinRuneForTCYStakeDistribution")
         val minRuneForTCYStakeDistribution: Long? = null,
