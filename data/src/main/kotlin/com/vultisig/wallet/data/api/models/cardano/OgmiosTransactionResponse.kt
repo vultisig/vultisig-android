671e8e3cfe9d63397745b4428bee94ed33aa7ade
Jaime Toca <jaime.toca.munoz@gmail.com>
2025-11-29 02:34:43 +0700

    [Fix]: Change Cardano Broadcast Endpoint (#2896)
    * change cardano broadcast
    
    * remove ID
    
    * log message
    
    * fix msg

---
+++ data/src/main/kotlin/com/vultisig/wallet/data/api/models/cardano/OgmiosTransactionResponse.kt
@@ -0,0 +1,29 @@
+package com.vultisig.wallet.data.api.models.cardano
+
+import com.vultisig.wallet.data.api.models.RpcError
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class OgmiosTransactionResponse(
+    @SerialName("jsonrpc")
+    val jsonrpc: String? = null,
+    @SerialName("method")
+    val method: String? = null,
+    @SerialName("result")
+    val result: OgmiosTransactionResult? = null,
+    @SerialName("error")
+    val error: RpcError? = null,
+)
+
+@Serializable
+data class OgmiosTransactionResult(
+    @SerialName("transaction")
+    val transaction: OgmiosTransaction,
+)
+
+@Serializable
+data class OgmiosTransaction(
+    @SerialName("id")
+    val id: String,
+)
