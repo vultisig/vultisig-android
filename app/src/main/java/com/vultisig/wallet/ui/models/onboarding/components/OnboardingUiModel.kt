09c3c4bb6147edf57167338bc83e83ac7595d59a
Amin <32006742+aminsato@users.noreply.github.com>
2025-12-11 08:35:15 +0330

    Refactor onboarding components to simplify state management and remove unused variables (#2961)
    Co-authored-by: hailhydra <johnnyluo1980@gmail.com>

--- app/src/main/java/com/vultisig/wallet/ui/models/onboarding/components/OnboardingUiModel.kt
+++ app/src/main/java/com/vultisig/wallet/ui/models/onboarding/components/OnboardingUiModel.kt
@@ -1,7 +1,6 @@
 package com.vultisig.wallet.ui.models.onboarding.components
 
 internal data class OnboardingUiModel(
-    val currentPage: OnboardingPage,
     val pageIndex: Int,
     val pageTotal: Int,
 )
