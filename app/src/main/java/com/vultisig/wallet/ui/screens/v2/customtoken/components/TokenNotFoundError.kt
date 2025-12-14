950f7af1500159d060d0a214d882053bd3d4e9a2
aminsato <amin.saradar@yahoo.com>
2025-12-14 17:07:16 +0330

    refactor: streamline UI components and improve layout in CustomTokenSearchBar and TokenNotFoundError

--- app/src/main/java/com/vultisig/wallet/ui/screens/v2/customtoken/components/TokenNotFoundError.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/v2/customtoken/components/TokenNotFoundError.kt
@@ -18,49 +18,58 @@ import androidx.compose.ui.unit.dp
 import com.vultisig.wallet.R
 import com.vultisig.wallet.ui.components.UiSpacer
 import com.vultisig.wallet.ui.components.buttons.VsButton
+import com.vultisig.wallet.ui.components.v2.containers.ContainerType
+import com.vultisig.wallet.ui.components.v2.containers.CornerType
+import com.vultisig.wallet.ui.components.v2.containers.V2Container
 import com.vultisig.wallet.ui.theme.Theme
 
 @Composable
 internal fun TokenNotFoundError(
     onRetryClick: () -> Unit,
 ) {
-    Column(
-        modifier = Modifier.Companion
-            .fillMaxWidth(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.Companion.CenterHorizontally
+    V2Container(
+        type = ContainerType.SECONDARY,
+        cornerType = CornerType.RoundedCornerShape(size = 12.dp),
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.iconcrypto),
-            contentDescription = "token not found",
-            modifier = Modifier.Companion.size(20.dp)
-        )
-        UiSpacer(size = 12.dp)
-        Text(
-            text = stringResource(R.string.custom_token_token_not_found),
-            color = Theme.v2.colors.text.primary,
-            style = Theme.brockmann.headings.title3
-        )
-        UiSpacer(size = 8.dp)
-        Text(
-            text = stringResource(R.string.custom_token_not_found_desc),
-            color = Theme.v2.colors.text.extraLight,
-            style = Theme.brockmann.supplementary.footnote,
-            textAlign = TextAlign.Companion.Center,
-            modifier = Modifier.Companion
-                .padding(
-                    horizontal = 50.dp
-                )
-        )
-        UiSpacer(
-            size = 16.dp
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.iconcrypto),
+                contentDescription = "token not found",
+                modifier = Modifier.size(20.dp)
+            )
+            UiSpacer(size = 12.dp)
+            Text(
+                text = stringResource(R.string.custom_token_token_not_found),
+                color = Theme.v2.colors.text.primary,
+                style = Theme.brockmann.headings.title3
+            )
+            UiSpacer(size = 8.dp)
+            Text(
+                text = stringResource(R.string.custom_token_not_found_desc),
+                color = Theme.v2.colors.text.extraLight,
+                style = Theme.brockmann.supplementary.footnote,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(
+                        horizontal = 50.dp
+                    )
+            )
+            UiSpacer(
+                size = 16.dp
+            )
 
-        VsButton(
-            label = stringResource(R.string.retry),
-            modifier = Modifier.Companion.fillMaxWidth(),
-            onClick = onRetryClick
-        )
+            VsButton(
+                label = stringResource(R.string.retry),
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onRetryClick
+            )
+        }
     }
 }
 
