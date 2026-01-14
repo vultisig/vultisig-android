e84d7a6d32dfb9a91e9b4e80368e2b4190973a8d
JaimeToca <jaime.toca.munoz@gmail.com>
2026-01-14 23:07:01 +0700

    fix

--- app/src/main/java/com/vultisig/wallet/ui/models/VaultRenameViewModel.kt
+++ app/src/main/java/com/vultisig/wallet/ui/models/VaultRenameViewModel.kt
@@ -1,6 +1,5 @@
 package com.vultisig.wallet.ui.models
 
-import android.content.Context
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.foundation.text.input.clearText
 import androidx.compose.foundation.text.input.setTextAndPlaceCursorAtEnd
@@ -12,16 +11,13 @@ import com.vultisig.wallet.R
 import com.vultisig.wallet.data.models.Vault
 import com.vultisig.wallet.data.repositories.VaultRepository
 import com.vultisig.wallet.data.usecases.IsVaultNameValid
-import com.vultisig.wallet.data.utils.TextFieldUtils
 import com.vultisig.wallet.ui.navigation.Destination
 import com.vultisig.wallet.ui.navigation.NavigationOptions
 import com.vultisig.wallet.ui.navigation.Navigator
 import com.vultisig.wallet.ui.navigation.Route
-import com.vultisig.wallet.ui.utils.SnackbarFlow
 import com.vultisig.wallet.ui.utils.UiText
 import com.vultisig.wallet.ui.utils.UiText.StringResource
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -37,9 +33,7 @@ internal data class VaultRenameUiModel(
 internal class VaultRenameViewModel @Inject constructor(
     private val vaultRepository: VaultRepository,
     private val navigator: Navigator<Destination>,
-    private val snackbarFlow: SnackbarFlow,
     private val isVaultNameValid: IsVaultNameValid,
-    @ApplicationContext private val context: Context,
     savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
     private val vaultId: String = savedStateHandle.toRoute<Route.Rename>().vaultId
