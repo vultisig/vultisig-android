796f8120eb91422b58550f12a9ec3ff357a401ee
JaimeToca <jaime.toca.munoz@gmail.com>
2026-01-15 17:21:49 +0700

    install plugin

--- app/src/main/java/com/vultisig/wallet/data/DataModule.kt
+++ app/src/main/java/com/vultisig/wallet/data/DataModule.kt
@@ -17,6 +17,7 @@ import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.HttpRequestRetry
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.cache.HttpCache
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
@@ -72,6 +73,8 @@ internal interface DataModule {
                 requestTimeoutMillis = 15000
                 connectTimeoutMillis = 15000
             }
+
+            install(HttpRequestRetry)
         }
 
         @Provides
