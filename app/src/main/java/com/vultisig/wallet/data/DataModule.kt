39f0a5166c7cd404947adccf622aaf1a6f41df9f
JaimeToca <jaime.toca.munoz@gmail.com>
2026-01-15 17:50:35 +0700

    implement retry with loop

--- app/src/main/java/com/vultisig/wallet/data/DataModule.kt
+++ app/src/main/java/com/vultisig/wallet/data/DataModule.kt
@@ -17,7 +17,6 @@ import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.DefaultRequest
-import io.ktor.client.plugins.HttpRequestRetry
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.cache.HttpCache
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
@@ -73,8 +72,6 @@ internal interface DataModule {
                 requestTimeoutMillis = 15000
                 connectTimeoutMillis = 15000
             }
-
-            install(HttpRequestRetry)
         }
 
         @Provides
