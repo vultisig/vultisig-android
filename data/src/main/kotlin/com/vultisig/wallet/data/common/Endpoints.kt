168b5fa850e7596a642e9c68b4149f0f4d1f5ea7
aminsato <amin.saradar@yahoo.com>
2026-02-24 10:35:41 +0330

    Update

--- data/src/main/kotlin/com/vultisig/wallet/data/common/Endpoints.kt
+++ data/src/main/kotlin/com/vultisig/wallet/data/common/Endpoints.kt
@@ -3,5 +3,5 @@ package com.vultisig.wallet.data.common
 object Endpoints {
     const val VULTISIG_RELAY_URL: String = "https://api.vultisig.com/router"
     const val LOCAL_MEDIATOR_SERVER_URL = "http://127.0.0.1:18080"
-    const val THORCHAIN_BROADCAST_TX: String = "https://thornode.ninerealms.com/cosmos/tx/v1beta1/txs"
+    const val THORCHAIN_BROADCAST_TX: String = "https://stagenet-thornode.ninerealms.com/cosmos/tx/v1beta1/txs"
 }
