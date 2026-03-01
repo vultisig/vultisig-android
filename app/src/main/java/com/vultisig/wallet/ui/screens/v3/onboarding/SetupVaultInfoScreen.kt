c65f378b085755c7a42ca69dfc7708fd7df3872a
aminsato <amin.saradar@yahoo.com>
2026-03-01 10:13:34 +0330

    Update SetupVaultInfoScreen.kt

--- app/src/main/java/com/vultisig/wallet/ui/screens/v3/onboarding/SetupVaultInfoScreen.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/v3/onboarding/SetupVaultInfoScreen.kt
@@ -10,9 +10,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.vultisig.wallet.R
 import com.vultisig.wallet.ui.components.UiSpacer
 import com.vultisig.wallet.ui.components.buttons.VsButton
 import com.vultisig.wallet.ui.components.v3.V3Scaffold
@@ -61,7 +63,7 @@ private fun SetupVaultInfoScreen(
                     )
             ) {
                 Text(
-                    text = "Your vault setup",
+                    text = stringResource(R.string.vault_setup_your_vault_setup),
                     color = Theme.v2.colors.neutrals.n50,
                     style = Theme.brockmann.headings.title2
                 )
@@ -115,7 +117,7 @@ private fun SetupVaultInfoScreen(
                 )
 
                 VsButton(
-                    label = "Get started",
+                    label = stringResource(R.string.key_import_device_count_get_started),
                     modifier = Modifier
                         .fillMaxWidth(),
                     onClick = {
