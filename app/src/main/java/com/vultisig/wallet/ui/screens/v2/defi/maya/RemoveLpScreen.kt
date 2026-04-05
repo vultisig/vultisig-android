cbe43d38f85798c9303c596c1de2832cfb3db2dc
aminsato <amin.saradar@yahoo.com>
2026-04-05 13:17:14 +0330

    Update RemoveLpScreen.kt

--- app/src/main/java/com/vultisig/wallet/ui/screens/v2/defi/maya/RemoveLpScreen.kt
+++ app/src/main/java/com/vultisig/wallet/ui/screens/v2/defi/maya/RemoveLpScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -182,6 +183,7 @@ private fun RemoveLpSlider(
         )
         UiSpacer(12.dp)
         Column(verticalArrangement = Arrangement.Center, modifier = Modifier.weight(1f)) {
+            Spacer(modifier = Modifier.height(4.dp))
             Slider(
                 value = percent,
                 onValueChange = onPercentChanged,
