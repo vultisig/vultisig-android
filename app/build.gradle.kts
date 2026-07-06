8c867fa53405720f1e7a1d4f0285adf6c93930af
hailhydra <johnnyluo1980@gmail.com>
2026-07-06 10:35:19 +1000

    chore: bump version to 1.0.113 (#5183)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.vultisig.wallet"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = 35
-        versionCode = 112
-        versionName = "1.0.112"
+        versionCode = 113
+        versionName = "1.0.113"
 
         testInstrumentationRunner = "com.vultisig.wallet.util.HiltTestRunner"
 
